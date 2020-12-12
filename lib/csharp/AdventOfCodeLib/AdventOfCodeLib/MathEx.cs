b215ac74b3e26deecca2bf4b86ece5dc6411cf67
Yorrick Spierings <yorrick.spierings@marel.com>
2020-12-12 12:48:19 +0100

    Challenge 2020.12 finished

---
+++ lib/csharp/AdventOfCodeLib/AdventOfCodeLib/MathEx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AdventOfCodeLib
+{
+	public static class MathEx
+	{
+		public static double ToRadians(double degrees) => (Math.PI / 180d) * degrees;
+
+		public static double ToDegrees(double radians) => (180d / Math.PI) * radians;
+
+		public static int Mod(int x, int d) => (x % d + d) % d;
+	}
+}
