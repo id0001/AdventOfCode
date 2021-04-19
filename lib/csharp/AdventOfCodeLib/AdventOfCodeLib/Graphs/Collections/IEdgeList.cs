0f4e3b3811101ec20d15a1fc0b9ae2354f5a80ce
Yorrick Spierings <emp637845@besi.com>
2021-04-19 14:16:34 +0200

    Removed old code

--- lib/csharp/AdventOfCodeLib/AdventOfCodeLib/Graphs/Collections/IEdgeList.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-
-namespace AdventOfCodeLib.Graphs.Collections
-{
-	public interface IEdgeList<TVertex, TEdge> : IList<TEdge> where TEdge : IEdge<TVertex>
-	{
-	}
-}
--- lib/csharp/AdventOfCodeLib/AdventOfCodeLib/IO/InputReaderOptions.cs
+++
@@ -1,8 +0,0 @@
-
-namespace AdventOfCodeLib.IO
-{
-	public class InputReaderOptions
-	{
-		public string InputFolder { get; set; }
-	}
-}
