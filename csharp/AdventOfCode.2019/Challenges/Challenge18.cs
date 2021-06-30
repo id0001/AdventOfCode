44c8b1da0e7946c7ae1638b52475829684391c97
Yorrick Spierings <emp637845@besi.com>
2021-06-30 10:46:40 +0200

    Added stuff, challenge 18 is not working yet

---
+++ csharp/AdventOfCode.2019/Challenges/Challenge18.cs
@@ -0,0 +1,238 @@
+﻿using AdventOfCode.Lib;
+using AdventOfCode.Lib.Graphs;
+using AdventOfCode.Lib.IO;
+using AdventOfCode.Lib.Pathfinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2019.Challenges
+{
+    [Challenge(18)]
+    public class Challenge18
+    {
+        private readonly IInputReader inputReader;
+        private char[,] map;
+
+
+        public Challenge18(IInputReader inputReader)
+        {
+            this.inputReader = inputReader;
+        }
+
+        [Setup]
+        public async Task SetupAsync()
+        {
+            var stringList = new List<string>();
+            await foreach (var line in inputReader.ReadLinesAsync(0))
+            {
+                stringList.Add(line);
+            }
+
+            map = new char[stringList.Count, stringList[0].Length];
+            for (int y = 0; y < map.GetLength(0); y++)
+            {
+                for (int x = 0; x < map.GetLength(1); x++)
+                {
+                    map[y, x] = stringList[y][x];
+                }
+            }
+        }
+
+        [Part1]
+        public string Part1()
+        {
+            var startPos = GetStartPosition(map);
+            var keys = GetKeys(map).ToList();
+            keys.Add(new Key(startPos, '@'));
+
+            var doors = GetDoors(map).ToDictionary(kv => kv.Location, kv => char.ToLowerInvariant(kv.Name));
+
+            var dsmap = GetDijkstraMap(map);
+
+            var graph = new BidirectionalGraph<Vertex, Edge>();
+
+            for (int i = 0; i < keys.Count; i++)
+            {
+                graph.AddVertex(new Vertex(keys[i].Location, char.ToLowerInvariant(keys[i].Name)));
+            }
+
+            var verts = graph.Vertices.ToList();
+            for (int y = 0; y < verts.Count; y++)
+            {
+                for (int x = y + 1; x < verts.Count; x++)
+                {
+                    var s = verts[y];
+                    var t = verts[x];
+
+                    var path = Dijkstra.Path(dsmap, s.Location, t.Location);
+
+                    var keysRequired = new List<char>();
+                    for (int i = 1; i < path.Length - 1; i++)
+                    {
+                        if (doors.TryGetValue(path[i], out char d))
+                        {
+                            keysRequired.Add(d);
+                        }
+                    }
+
+                    graph.AddEdge(new Edge(s, t, keysRequired.ToHashSet(), path.Length - 1));
+                }
+            }
+
+            return null;
+        }
+
+        [Part2]
+        public string Part2()
+        {
+            return null;
+        }
+
+        private sealed record Node(Vertex Vertex, int Distance, char[] KeysFound)
+        {
+            public bool Equals(Node other) => Vertex == other.Vertex;
+
+            public override int GetHashCode() => Vertex.GetHashCode();
+        }
+
+        private static Node Bfs(BidirectionalGraph<Vertex, Edge> graph, char[] keysToFind)
+        {
+            var startVert = graph.Vertices.Single(e => e.Character == '@');
+            var root = new Node(startVert, 0, new char[0]);
+
+            var queue = new Queue<Node>();
+            var explored = new HashSet<Vertex>();
+            explored.Add(root);
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+
+                if (node.KeysFound.Length == keysToFind.Length)
+                {
+                    return node;
+                }
+
+                foreach(var edge in graph.OutEdges(node.Vertex))
+                {
+                    if (!explored.Contains(edge.Target))
+                    {
+
+                    }
+                }
+            }
+
+
+            throw new InvalidOperationException("No path found");
+        }
+
+        private void PrintMap()
+        {
+            for (int y = 0; y < 81; y++)
+            {
+                for (int x = 0; x < 81; x++)
+                {
+                    char c = map[y, x];
+
+                    Console.ResetColor();
+                    if (c != '#' && c != '.' && c != ',')
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+
+                    if (c == ',')
+                        Console.BackgroundColor = ConsoleColor.Red;
+
+                    Console.Write(c);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static Point2 GetStartPosition(char[,] map)
+        {
+            for (int y = 0; y < map.GetLength(0); y++)
+            {
+                for (int x = 0; x < map.GetLength(1); x++)
+                {
+                    if (map[y, x] == '@')
+                        return new Point2(x, y);
+                }
+            }
+
+            return Point2.Zero;
+        }
+
+        private static IEnumerable<Key> GetKeys(char[,] map)
+        {
+            for (int y = 0; y < map.GetLength(0); y++)
+            {
+                for (int x = 0; x < map.GetLength(1); x++)
+                {
+                    if (map[y, x] != '@' && map[y, x] != '#' && map[y, x] != '.' && char.IsLower(map[y, x]))
+                        yield return new Key(new Point2(x, y), char.ToLower(map[y, x]));
+                }
+            }
+        }
+
+        private static IEnumerable<Door> GetDoors(char[,] map)
+        {
+            for (int y = 0; y < map.GetLength(0); y++)
+            {
+                for (int x = 0; x < map.GetLength(1); x++)
+                {
+                    if (map[y, x] != '@' && map[y, x] != '#' && map[y, x] != '.' && char.IsUpper(map[y, x]))
+                        yield return new Door(new Point2(x, y), char.ToLower(map[y, x]));
+                }
+            }
+        }
+
+        private IDictionary<Point2, bool> GetDijkstraMap(char[,] map)
+        {
+            var dict = new Dictionary<Point2, bool>();
+            for (int y = 0; y < 81; y++)
+            {
+                for (int x = 0; x < 81; x++)
+                {
+                    dict.Add(new Point2(x, y), map[y, x] != '#');
+                }
+            }
+
+            return dict;
+        }
+
+        private record Key(Point2 Location, char Name);
+
+        private record Door(Point2 Location, char Name);
+
+        private class Vertex
+        {
+            public Vertex(Point2 location, char character)
+            {
+                Location = location;
+                Character = character;
+            }
+
+            public Point2 Location { get; }
+
+            public char Character { get; }
+        }
+
+        private class Edge : Edge<Vertex>
+        {
+            public Edge(Vertex source, Vertex target, HashSet<char> keysRequired, int distance) : base(source, target)
+            {
+                KeysRequired = keysRequired;
+                Distance = distance;
+            }
+
+            public HashSet<char> KeysRequired { get; }
+
+            public int Distance { get; }
+        }
+    }
+}
--- csharp/AdventOfCode.Lib/Pathfinding/Dijkstra.cs
+++ csharp/AdventOfCode.Lib/Pathfinding/Dijkstra.cs
@@ -1,140 +1,130 @@
 
 using AdventOfCode.Lib.Collections;
+using Microsoft;
 using System;
 using System.Collections.Generic;
 
 
 namespace AdventOfCode.Lib.Pathfinding
 {
-	public class Dijkstra
-	{
-		/// <summary>
-		/// Calculate the shortest path from start to end.
-		/// </summary>
-		/// <param name="graph">The graph with known nodes</param>
-		/// <param name="start">The start node</param>
-		/// <param name="end">The target node</param>
-		/// <returns>An array containing the path to target. Excludes the start node. If not path was found, and empty array is returned.</returns>
-		public static Point2[] Path(IDictionary<Point2, bool> graph, Point2 start, Point2 end)
-		{
-			if (graph == null)
-				throw new ArgumentNullException(nameof(graph));
+    public class Dijkstra
+    {
+        /// <summary>
+        /// Calculate the shortest path from start to end.
+        /// </summary>
+        /// <param name="graph">The graph with known nodes</param>
+        /// <param name="start">The start node</param>
+        /// <param name="end">The target node</param>
+        /// <returns>An array containing the path to target. Excludes the start node. If not path was found, and empty array is returned.</returns>
+        public static Point2[] Path(IDictionary<Point2, bool> graph, Point2 start, Point2 end)
+        {
+            Requires.NotNull(graph, nameof(graph));
+            Requires.Argument(start != end, nameof(start), "Start cannot be the same as finish");
+            Requires.Argument(graph.ContainsKey(start), nameof(start), "Start location is not in the graph");
+            Requires.Argument(graph[start], nameof(start), "Start location is not traversable");
+            Requires.Argument(graph.ContainsKey(end), nameof(end), "End location is not in the graph");
+            Requires.Argument(graph[end], nameof(end), "End location is not traversable");
 
-			if (start == end)
-				throw new ArgumentException("Start cannot be the same as finish");
+            var distances = new Dictionary<Point2, int>();
+            var previous = new Dictionary<Point2, Point2>();
+            var openQueue = new PriorityQueue<Point2>(new MinDistComparer(distances));
+            var visited = new HashSet<Point2>();
 
-			if (!graph.ContainsKey(start))
-				throw new ArgumentException("Start location is not in the graph");
+            distances[start] = 0;
+            openQueue.Enqueue(start);
 
-			if (!graph[start])
-				throw new ArgumentException("Start location is not traversable");
+            while (!openQueue.IsEmpty)
+            {
+                var node = openQueue.Dequeue();
 
-			if (!graph.ContainsKey(end))
-				throw new ArgumentException("End location is not in the graph");
+                visited.Add(node);
 
-			if (!graph[end])
-				throw new ArgumentException("End location is not traversable");
+                if (node == end)
+                    break;
 
-			var distances = new Dictionary<Point2, int>();
-			var previous = new Dictionary<Point2, Point2>();
-			var openQueue = new PriorityQueue<Point2>(new MinDistComparer(distances));
-			var visited = new HashSet<Point2>();
+                var neighbors = GetNeighbours(graph, node);
+                foreach (var neighbor in neighbors)
+                {
+                    int dist = distances[node] + 1;
+                    if (!distances.ContainsKey(neighbor) || dist < distances[neighbor])
+                    {
+                        distances[neighbor] = dist;
+                        previous[neighbor] = node;
 
-			distances[start] = 0;
-			openQueue.Enqueue(start);
+                        if (!visited.Contains(neighbor))
+                            openQueue.Enqueue(neighbor);
+                    }
+                }
+            }
 
-			while (!openQueue.IsEmpty)
-			{
-				var node = openQueue.Dequeue();
+            if (!visited.Contains(end))
+                return new Point2[0];
 
-				visited.Add(node);
+            var path = new List<Point2>();
+            Point2 p = end;
+            while (p != start)
+            {
+                path.Insert(0, p);
+                p = previous[p];
+            }
 
-				if (node == end)
-					break;
+            return path.ToArray();
+        }
 
-				var neighbors = GetNeighbours(graph, node);
-				foreach (var neighbor in neighbors)
-				{
-					int dist = distances[node] + 1;
-					if (!distances.ContainsKey(neighbor) || dist < distances[neighbor])
-					{
-						distances[neighbor] = dist;
-						previous[neighbor] = node;
+        /// <summary>
+        /// Calculate the shortest path from start to end.
+        /// </summary>
+        /// <param name="graph">The graph with known nodes</param>
+        /// <param name="start">The start node</param>
+        /// <param name="end">The target node</param>
+        /// <param name="path">An array containing the path to target. Excludes the start node.</param>
+        /// <returns>True if a path was found. False if not</returns>
+        public static bool TryPath(IDictionary<Point2, bool> graph, Point2 start, Point2 end, out Point2[] path)
+        {
+            path = Path(graph, start, end);
+            return path.Length != 0;
+        }
 
-						if (!visited.Contains(neighbor))
-							openQueue.Enqueue(neighbor);
-					}
-				}
-			}
+        /// <summary>
+        /// Get the neighbour nodes.
+        /// </summary>
+        /// <param name="graph">The graph with known  nodes</param>
+        /// <param name="parent">The parent node.</param>
+        /// <returns>A list of neighbour nodes.</returns>
+        private static IEnumerable<Point2> GetNeighbours(IDictionary<Point2, bool> graph, Point2 parent)
+        {
+            List<Point2> neighbours = new List<Point2>();
+            for (int y = parent.Y - 1; y <= parent.Y + 1; y++)
+            {
+                for (int x = parent.X - 1; x <= parent.X + 1; x++)
+                {
+                    if (x == parent.X ^ y == parent.Y && graph.TryGetValue(parent, out bool v) && v)
+                    {
+                        var p = new Point2(x, y);
+                        neighbours.Add(p);
+                    }
+                }
+            }
 
-			if (!visited.Contains(end))
-				return new Point2[0];
+            return neighbours;
+        }
 
-			var path = new List<Point2>();
-			Point2 p = end;
-			while (p != start)
-			{
-				path.Insert(0, p);
-				p = previous[p];
-			}
+        private class MinDistComparer : IComparer<Point2>
+        {
+            private readonly IDictionary<Point2, int> distances;
 
-			return path.ToArray();
-		}
+            public MinDistComparer(IDictionary<Point2, int> distances)
+            {
+                this.distances = distances;
+            }
 
-		/// <summary>
-		/// Calculate the shortest path from start to end.
-		/// </summary>
-		/// <param name="graph">The graph with known nodes</param>
-		/// <param name="start">The start node</param>
-		/// <param name="end">The target node</param>
-		/// <param name="path">An array containing the path to target. Excludes the start node.</param>
-		/// <returns>True if a path was found. False if not</returns>
-		public static bool TryPath(IDictionary<Point2, bool> graph, Point2 start, Point2 end, out Point2[] path)
-		{
-			path = Path(graph, start, end);
-			return path.Length != 0;
-		}
+            public int Compare(Point2 x, Point2 y)
+            {
+                if (!distances.ContainsKey(x) || !distances.ContainsKey(y))
+                    throw new InvalidOperationException("Unable to compare distance of unknown points.");
 
-		/// <summary>
-		/// Get the neighbour nodes.
-		/// </summary>
-		/// <param name="graph">The graph with known  nodes</param>
-		/// <param name="parent">The parent node.</param>
-		/// <returns>A list of neighbour nodes.</returns>
-		private static IEnumerable<Point2> GetNeighbours(IDictionary<Point2, bool> graph, Point2 parent)
-		{
-			List<Point2> neighbours = new List<Point2>();
-			for (int y = parent.Y - 1; y <= parent.Y + 1; y++)
-			{
-				for (int x = parent.X - 1; x <= parent.X + 1; x++)
-				{
-					if (x == parent.X ^ y == parent.Y && graph.TryGetValue(parent, out bool v) && v)
-					{
-						var p = new Point2(x, y);
-						neighbours.Add(p);
-					}
-				}
-			}
-
-			return neighbours;
-		}
-
-		private class MinDistComparer : IComparer<Point2>
-		{
-			private readonly IDictionary<Point2, int> distances;
-
-			public MinDistComparer(IDictionary<Point2, int> distances)
-			{
-				this.distances = distances;
-			}
-
-			public int Compare(Point2 x, Point2 y)
-			{
-				if (!distances.ContainsKey(x) || !distances.ContainsKey(y))
-					throw new InvalidOperationException("Unable to compare distance of unknown points.");
-
-				return distances[x] - distances[y];
-			}
-		}
-	}
+                return distances[x] - distances[y];
+            }
+        }
+    }
 }
