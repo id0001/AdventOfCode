6c19263b156ef7b305bbd2ba595d98039f956be4
Yorrick Spierings <yi.spierings@gmail.com>
2021-12-01 08:48:30 +0100

    Day 1 fsharp

---
+++ fsharp/AdventOfCode.2021/AdventOfCode.2021/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+open Challenge01
+
+let executePart part fn =
+    printfn "Part %d: %s" part fn
+
+[<EntryPoint>]
+let main argv =
+    executePart 1 part1
+    executePart 2 part2
+
+    printfn ""
+    printfn "Press any key to continue..."
+    Console.ReadKey true |> ignore
+    0
