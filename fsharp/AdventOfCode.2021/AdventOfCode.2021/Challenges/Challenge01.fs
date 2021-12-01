3added35cbe3ee2540330205de0bd7381419869b
Yorrick Spierings <yi.spierings@gmail.com>
2021-12-01 08:48:30 +0100

    Day 1 fsharp

---
+++ fsharp/AdventOfCode.2021/AdventOfCode.2021/Challenges/Challenge01.fs
@@ -0,0 +1,21 @@
+﻿module Challenge01
+
+open Utils.IO
+
+let cmp pair = fst pair < snd pair
+
+let part1 =
+    readLines<int> 1
+        |> Seq.pairwise
+        |> Seq.filter cmp
+        |> Seq.length
+        |> string
+
+let part2 =
+    readLines<int> 1
+        |> Seq.windowed 3
+        |> Seq.map Array.sum
+        |> Seq.pairwise
+        |> Seq.filter cmp
+        |> Seq.length
+        |> string
---
+++ fsharp/AdventOfCode.2021/AdventOfCode.2021/Utils/IO.fs
@@ -0,0 +1,14 @@
+﻿module Utils.IO
+
+open System
+open System.IO
+
+let InputFolder = "Inputs"
+
+let private challenge2Filename challenge =
+    Path.Combine [| InputFolder; sprintf "%02d.txt" challenge |]
+
+let readLines<'T> challenge =
+    let filename = challenge2Filename challenge
+    File.ReadAllLines(filename)
+        |> Seq.map (fun x -> Convert.ChangeType(x, typeof<'T>) :?> 'T)
