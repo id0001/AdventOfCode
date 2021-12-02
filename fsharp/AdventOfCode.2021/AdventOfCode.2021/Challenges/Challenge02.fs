44357f5ff007eebd7eb2afa413e7d90931c14d45
Yorrick Spierings <yi.spierings@gmail.com>
2021-12-02 09:30:12 +0100

    Improved f# solution

--- fsharp/AdventOfCode.2021/AdventOfCode.2021/Challenges/Challenge02.fs
+++ fsharp/AdventOfCode.2021/AdventOfCode.2021/Challenges/Challenge02.fs
@@ -3,41 +3,81 @@
 module Challenge02 =
 
     open Utils.IO
-    open System
 
-    type Movement = { Direction:string; Amount:int }
+    // Old code:
 
-    let mapMovement (line:string )=
-        line.Split(" ", StringSplitOptions.RemoveEmptyEntries ||| StringSplitOptions.TrimEntries)
-            |> fun x -> { Movement.Direction = x.[0]; Movement.Amount = int(x.[1]) }
+    //type Movement = { Direction:string; Amount:int }
 
-    let readInput =
-        readLines 2
-            |> Seq.map mapMovement
+    //let mapMovement (line:string )=
+    //    line.Split(" ", StringSplitOptions.RemoveEmptyEntries ||| StringSplitOptions.TrimEntries)
+    //        |> fun x -> { Movement.Direction = x.[0]; Movement.Amount = int(x.[1]) }
 
-    let move (m:Movement) =
-        match m.Direction with
-        | "forward" -> (m.Amount,0)
-        | "down"-> (0,m.Amount)
-        | "up" -> (0,-m.Amount)
-        | _ -> raise (NotImplementedException ())
+    //let readInput =
+    //    readLines 2
+    //        |> Seq.map mapMovement
 
-    let fold1 (x,y) (m:Movement) =
-        let (dx,dy) = move m
-        (x+dx,y+dy)
+    //let move (m:Movement) =
+    //    match m.Direction with
+    //    | "forward" -> (m.Amount,0)
+    //    | "down"-> (0,m.Amount)
+    //    | "up" -> (0,-m.Amount)
+    //    | _ -> raise (NotImplementedException ())
 
-    let fold2 (x,y,aim) (m:Movement) =
-        let (dx,daim) = move m
-        (x+dx,y + dx*aim,aim + daim)
+    //let fold1 (x,y) (m:Movement) =
+    //    let (dx,dy) = move m
+    //    (x+dx,y+dy)
+
+    //let fold2 (x,y,aim) (m:Movement) =
+    //    let (dx,daim) = move m
+    //    (x+dx,y + dx*aim,aim + daim)
+
+    //let part1 =
+    //    readInput
+    //        |> Seq.fold fold1 (0,0)
+    //        |> fun (x,y) -> x*y
+    //        |> string
+
+    //let part2 =
+    //    readInput
+    //    |> Seq.fold fold2 (0,0,0)
+    //    |> fun (x,y,_) -> x*y
+    //    |> string
+
+    // New improved code:
+
+    type Movement =
+        | Horizontal of int
+        | Vertical of int
+
+    let parseInput (line:string) =
+        match line.Split " " with
+        | [| "forward"; v |] -> Horizontal (int v)
+        | [| "up"; v |] -> Vertical (-int v)
+        | [| "down"; v |] -> Vertical (int v)
+        | _ -> failwithf "Unsupported input command: %s" line
 
     let part1 =
-        readInput
-            |> Seq.fold fold1 (0,0)
+        readLines 2
+            |> Seq.map parseInput
+            |> Seq.fold
+                (fun (x,y) cmd ->
+                    match cmd with
+                    | Horizontal v -> (x+v,y)
+                    | Vertical v -> (x,y+v)
+                )
+                (0,0)
             |> fun (x,y) -> x*y
             |> string
 
     let part2 =
-        readInput
-        |> Seq.fold fold2 (0,0,0)
-        |> fun (x,y,_) -> x*y
-        |> string
+        readLines 2
+            |> Seq.map parseInput
+            |> Seq.fold
+                (fun (x,y,aim) cmd ->
+                    match cmd with
+                    | Horizontal v -> (x+v,y+(v*aim),aim)
+                    | Vertical v -> (x,y,aim + v)
+                )
+                (0,0,0)
+            |> fun (x,y,_) -> x*y
+            |> string
