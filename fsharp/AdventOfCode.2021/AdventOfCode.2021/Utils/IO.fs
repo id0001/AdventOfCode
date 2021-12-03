ecddde694ae3671882405e3485e8d90f7c05e356
Yorrick Spierings <yi.spierings@gmail.com>
2021-12-03 12:09:44 +0100

    Clean up code

--- fsharp/AdventOfCode.2021/AdventOfCode.2021/Utils/IO.fs
+++ fsharp/AdventOfCode.2021/AdventOfCode.2021/Utils/IO.fs
@@ -1,16 +1,14 @@
-﻿namespace Utils
+﻿module Utils.IO
 
-module IO =
+open System
+open System.IO
 
-    open System
-    open System.IO
+let InputFolder = "Inputs"
 
-    let InputFolder = "Inputs"
+let private challenge2Filename challenge =
+    Path.Combine [| InputFolder; sprintf "%02d.txt" challenge |]
 
-    let private challenge2Filename challenge =
-        Path.Combine [| InputFolder; sprintf "%02d.txt" challenge |]
-
-    let readLines<'T> challenge =
-        let filename = challenge2Filename challenge
-        File.ReadAllLines(filename)
-            |> Seq.map (fun x -> Convert.ChangeType(x, typeof<'T>) :?> 'T)
+let readLines<'T> challenge =
+    let filename = challenge2Filename challenge
+    File.ReadAllLines(filename)
+        |> Seq.map (fun x -> Convert.ChangeType(x, typeof<'T>) :?> 'T)
