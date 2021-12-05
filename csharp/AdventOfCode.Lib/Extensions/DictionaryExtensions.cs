38c6349ec7c47354d732834479f49549c192d4b4
= <yi.spierings@gmail.com>
2021-12-05 07:59:53 +0100

    Day 5 c# is done

---
+++ csharp/AdventOfCode.Lib/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Lib.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        public static void AddOrUpdate<T, K>(this IDictionary<T, K> source, T key, K value)
+        {
+            Requires.NotNull(source, nameof(source));
+
+            if (!source.TryAdd(key, value))
+                source[key] = value;
+        }
+
+        public static void Update<T, K>(this IDictionary<T, K> source, T key, Func<K, K> update, K defaultValue = default)
+        {
+            Requires.NotNull(source, nameof(source));
+            Requires.NotNull(update, nameof(update));
+
+            if (!source.TryGetValue(key, out K oldValue))
+            {
+                source.Add(key, update(defaultValue));
+            }
+            else
+            {
+                source[key] = update(oldValue);
+            }
+        }
+    }
+}
