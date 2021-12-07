09bead9d49b1667008350d8258bfc46145f923b9
Yorrick Spierings <yi.spierings@gmail.com>
2021-12-07 07:47:28 +0100

    Day 7 c# is finished.

---
+++ csharp/AdventOfCode.2021/Challenges/Challenge07.cs
@@ -0,0 +1,60 @@
+﻿using AdventOfCode.Lib;
+using AdventOfCode.Lib.IO;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Challenges
+{
+    [Challenge(7)]
+    public class Challenge07
+    {
+        private readonly IInputReader inputReader;
+        private int[] data;
+
+        public Challenge07(IInputReader inputReader)
+        {
+            this.inputReader = inputReader;
+        }
+
+        [Setup]
+        public async Task SetupAsync()
+        {
+            data = await inputReader.ReadLineAsync<int>(7, ',').ToArrayAsync();
+        }
+
+        [Part1]
+        public string Part1()
+        {
+            int min = data.Min();
+            int max = data.Max();
+
+            int lowest = int.MaxValue;
+            for (int pos = min; pos < max; pos++)
+            {
+                int test = data.Select(x => Math.Abs(x - pos)).Sum();
+                if (test < lowest)
+                    lowest = test;
+            }
+
+            return lowest.ToString();
+        }
+
+        [Part2]
+        public string Part2()
+        {
+            int min = data.Min();
+            int max = data.Max();
+
+            int lowest = int.MaxValue;
+            for (int pos = min; pos < max; pos++)
+            {
+                int test = data.Select(x => MathEx.TriangularNumber(Math.Abs(x - pos))).Sum();
+                if (test < lowest)
+                    lowest = test;
+            }
+
+            return lowest.ToString();
+        }
+    }
+}
