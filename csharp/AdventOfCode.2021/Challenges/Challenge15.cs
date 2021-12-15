ac73f141ee6e15692c02137346640de57c512140
Yorrick Spierings <yi.spierings@gmail.com>
2021-12-15 06:42:54 +0100

    Day 15 c# is done. Fixed a bug printing the elapsed time taken.

---
+++ csharp/AdventOfCode.2021/Challenges/Challenge15.cs
@@ -0,0 +1,83 @@
+﻿using AdventOfCode.Lib;
+using AdventOfCode.Lib.IO;
+using AdventOfCode.Lib.Pathfinding;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Challenges
+{
+    [Challenge(15)]
+    public class Challenge15
+    {
+        private readonly IInputReader inputReader;
+        private int[,] grid;
+
+        public Challenge15(IInputReader inputReader)
+        {
+            this.inputReader = inputReader;
+        }
+
+        [Setup]
+        public async Task SetupAsync()
+        {
+            grid = await inputReader.ReadGridAsync<int>(15);
+        }
+
+        [Part1]
+        public string Part1()
+        {
+            var dijkstra = new Dijkstra<Point2>(GetAdjecentNodes);
+            if (dijkstra.TryPath(new Point2(0, 0), new Point2(grid.GetLength(0) - 1, grid.GetLength(1) - 1), out Point2[] path, out int shortest))
+            {
+                return shortest.ToString();
+            }
+
+            return null;
+        }
+
+        [Part2]
+        public string Part2()
+        {
+            var dijkstra = new Dijkstra<Point2>(GetAdjecentNodes2);
+            if (dijkstra.TryPath(new Point2(0, 0), new Point2((grid.GetLength(0) * 5) - 1, (grid.GetLength(1) * 5) - 1), out Point2[] path, out int shortest))
+            {
+                return shortest.ToString();
+            }
+
+            return null;
+        }
+
+        private IEnumerable<(Point2, int)> GetAdjecentNodes(Point2 currentNode)
+        {
+            foreach (var neighbor in currentNode.GetNeighbors4())
+            {
+                if (neighbor.X < 0 || neighbor.Y < 0 || neighbor.X >= grid.GetLength(1) || neighbor.Y >= grid.GetLength(0))
+                    continue;
+
+                yield return (neighbor, grid[neighbor.Y, neighbor.X]);
+            }
+        }
+
+        private IEnumerable<(Point2, int)> GetAdjecentNodes2(Point2 currentNode)
+        {
+            int width = grid.GetLength(1) * 5;
+            int height = grid.GetLength(0) * 5;
+
+            foreach (var neighbor in currentNode.GetNeighbors4())
+            {
+                if (neighbor.X < 0 || neighbor.Y < 0 || neighbor.X >= width || neighbor.Y >= height)
+                    continue;
+
+                int rx = neighbor.X % grid.GetLength(1);
+                int ry = neighbor.Y % grid.GetLength(0);
+                int fx = neighbor.X / grid.GetLength(1);
+                int fy = neighbor.Y / grid.GetLength(0);
+                int risk = ((grid[ry, rx] + fx + fy - 1) % 9) + 1;
+
+                yield return (neighbor, risk);
+            }
+        }
+
+        private record Node(Point2 Location, int Weight);
+    }
+}
--- csharp/AdventOfCode.Lib/ChallengeHost.cs
+++ csharp/AdventOfCode.Lib/ChallengeHost.cs
@@ -13,241 +13,250 @@ using System.Threading.Tasks;
 namespace AdventOfCode.Lib
 {
     internal static class ChallengeHelper
-	{
-		public static async Task SetupAsync(object challenge)
-		{
-			Type type = challenge.GetType();
+    {
+        public static async Task SetupAsync(object challenge)
+        {
+            Type type = challenge.GetType();
 
-			var methods = type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.InvokeMethod | BindingFlags.DeclaredOnly);
-			var runMethod = methods.FirstOrDefault(m => m.GetCustomAttribute<SetupAttribute>() != null);
-			if (runMethod != null)
-			{
-				var result = runMethod.Invoke(challenge, null);
-				if (result != null && result is Task t)
-				{
-					await t;
-				}
-			}
-		}
+            var methods = type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.InvokeMethod | BindingFlags.DeclaredOnly);
+            var runMethod = methods.FirstOrDefault(m => m.GetCustomAttribute<SetupAttribute>() != null);
+            if (runMethod != null)
+            {
+                var result = runMethod.Invoke(challenge, null);
+                if (result != null && result is Task t)
+                {
+                    await t;
+                }
+            }
+        }
 
-		public static async Task<string> Part1Async(object challenge)
-		{
-			Type type = challenge.GetType();
+        public static async Task<string> Part1Async(object challenge)
+        {
+            Type type = challenge.GetType();
 
-			var methods = type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.InvokeMethod | BindingFlags.DeclaredOnly);
-			var runMethod = methods.FirstOrDefault(m => m.GetCustomAttribute<Part1Attribute>() != null);
-			if (runMethod != null)
-			{
-				var result = runMethod.Invoke(challenge, null);
-				if (result != null && result is Task<string> t)
-				{
-					return await t;
-				}
+            var methods = type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.InvokeMethod | BindingFlags.DeclaredOnly);
+            var runMethod = methods.FirstOrDefault(m => m.GetCustomAttribute<Part1Attribute>() != null);
+            if (runMethod != null)
+            {
+                var result = runMethod.Invoke(challenge, null);
+                if (result != null && result is Task<string> t)
+                {
+                    return await t;
+                }
 
-				return (string)result;
-			}
+                return (string)result;
+            }
 
-			return null;
-		}
+            return null;
+        }
 
-		public static async Task<string> Part2Async(object challenge)
-		{
-			Type type = challenge.GetType();
+        public static async Task<string> Part2Async(object challenge)
+        {
+            Type type = challenge.GetType();
 
-			var methods = type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.InvokeMethod | BindingFlags.DeclaredOnly);
-			var runMethod = methods.FirstOrDefault(m => m.GetCustomAttribute<Part2Attribute>() != null);
-			if (runMethod != null)
-			{
-				var result = runMethod.Invoke(challenge, null);
-				if (result != null && result is Task<string> t)
-				{
-					return await t;
-				}
+            var methods = type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.InvokeMethod | BindingFlags.DeclaredOnly);
+            var runMethod = methods.FirstOrDefault(m => m.GetCustomAttribute<Part2Attribute>() != null);
+            if (runMethod != null)
+            {
+                var result = runMethod.Invoke(challenge, null);
+                if (result != null && result is Task<string> t)
+                {
+                    return await t;
+                }
 
-				return (string)result;
-			}
+                return (string)result;
+            }
 
-			return null;
-		}
-	}
+            return null;
+        }
+    }
 
-	public class ChallengeHost : IHostedService
-	{
-		private readonly IServiceProvider serviceProvider;
-		private readonly ILogger logger;
-		private readonly IHostApplicationLifetime hostApplicationLifetime;
-		private readonly IChallengeTypeProvider challengeTypeProvider;
+    public class ChallengeHost : IHostedService
+    {
+        private readonly IServiceProvider serviceProvider;
+        private readonly ILogger logger;
+        private readonly IHostApplicationLifetime hostApplicationLifetime;
+        private readonly IChallengeTypeProvider challengeTypeProvider;
 
-		private ErrorCode exitCode = ErrorCode.Success;
+        private ErrorCode exitCode = ErrorCode.Success;
 
-		public ChallengeHost(IServiceProvider serviceProvider, ILogger<ChallengeHost> logger, IHostApplicationLifetime hostApplicationLifetime, IChallengeTypeProvider challengeTypeProvider)
-		{
-			this.serviceProvider = serviceProvider;
-			this.logger = logger;
-			this.hostApplicationLifetime = hostApplicationLifetime;
-			this.challengeTypeProvider = challengeTypeProvider;
+        public ChallengeHost(IServiceProvider serviceProvider, ILogger<ChallengeHost> logger, IHostApplicationLifetime hostApplicationLifetime, IChallengeTypeProvider challengeTypeProvider)
+        {
+            this.serviceProvider = serviceProvider;
+            this.logger = logger;
+            this.hostApplicationLifetime = hostApplicationLifetime;
+            this.challengeTypeProvider = challengeTypeProvider;
 
-			hostApplicationLifetime.ApplicationStarted.Register(async () => await OnApplicationStarted());
-		}
+            hostApplicationLifetime.ApplicationStarted.Register(async () => await OnApplicationStarted());
+        }
 
-		public Task StartAsync(CancellationToken cancellationToken)
-		{
-			logger.LogDebug($"Starting with arguments: {string.Join(" ", Environment.GetCommandLineArgs())}");
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            logger.LogDebug($"Starting with arguments: {string.Join(" ", Environment.GetCommandLineArgs())}");
 
-			return Task.CompletedTask;
-		}
+            return Task.CompletedTask;
+        }
 
-		public Task StopAsync(CancellationToken cancellationToken)
-		{
-			Environment.ExitCode = (int)exitCode;
-			return Task.CompletedTask;
-		}
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Environment.ExitCode = (int)exitCode;
+            return Task.CompletedTask;
+        }
 
-		private async Task OnApplicationStarted()
-		{
-			try
-			{
-				logger.LogDebug("Application started.");
+        private async Task OnApplicationStarted()
+        {
+            try
+            {
+                logger.LogDebug("Application started.");
 
-				var args = Environment.GetCommandLineArgs().Skip(1).ToArray(); // Remove application path
-				var usage = Resources.Docopt;
-				var arguments = new Docopt().Apply(usage, args, true, "1.0", true, false);
-				if (arguments == null)
-				{
-					return;
-				}
+                var args = Environment.GetCommandLineArgs().Skip(1).ToArray(); // Remove application path
+                var usage = Resources.Docopt;
+                var arguments = new Docopt().Apply(usage, args, true, "1.0", true, false);
+                if (arguments == null)
+                {
+                    return;
+                }
 
-				if (arguments["--challenge"] != null)
-				{
-					if (!arguments["--challenge"].IsInt)
-					{
-						logger.LogError("<day> argument must be an integer.");
-						exitCode = ErrorCode.InvalidStartupArgument;
-						return;
-					}
+                if (arguments["--challenge"] != null)
+                {
+                    if (!arguments["--challenge"].IsInt)
+                    {
+                        logger.LogError("<day> argument must be an integer.");
+                        exitCode = ErrorCode.InvalidStartupArgument;
+                        return;
+                    }
 
-					await RunChallenge(arguments["--challenge"].AsInt);
-				}
-				else if (arguments["--latest"].IsTrue)
-				{
-					await RunLatest();
-				}
-				else if (arguments["--all"].IsTrue)
-				{
-					await RunAll();
-				}
-			}
-			catch (DocoptInputErrorException ex)
-			{
-				Console.WriteLine(ex.Message);
-				await Task.Delay(3000);
-			}
-			catch (Exception ex)
-			{
-				logger.LogError(ex, "Unhandled exception.");
-			}
-			finally
-			{
-				hostApplicationLifetime.StopApplication();
-			}
-		}
+                    await RunChallenge(arguments["--challenge"].AsInt);
+                }
+                else if (arguments["--latest"].IsTrue)
+                {
+                    await RunLatest();
+                }
+                else if (arguments["--all"].IsTrue)
+                {
+                    await RunAll();
+                }
+            }
+            catch (DocoptInputErrorException ex)
+            {
+                Console.WriteLine(ex.Message);
+                await Task.Delay(3000);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Unhandled exception.");
+            }
+            finally
+            {
+                hostApplicationLifetime.StopApplication();
+            }
+        }
 
-		private async Task RunChallenge(int day)
-		{
-			var sw = new Stopwatch();
+        private async Task RunChallenge(int day)
+        {
+            var sw = new Stopwatch();
 
-			if (!challengeTypeProvider.TryGetValue(day, out Type challengeType))
-			{
-				logger.LogWarning($"Challenge for day {day} does not exist.");
-				return;
-			}
+            if (!challengeTypeProvider.TryGetValue(day, out Type challengeType))
+            {
+                logger.LogWarning($"Challenge for day {day} does not exist.");
+                return;
+            }
 
-			Console.WriteLine($"Day {day}:");
+            Console.WriteLine($"Day {day}:");
 
-			sw.Start();
-			string part1 = await ExecutePart1(challengeType);
-			sw.Stop();
+            sw.Start();
+            string part1 = await ExecutePart1(challengeType);
+            sw.Stop();
 
-			if (!string.IsNullOrEmpty(part1))
-				Console.WriteLine($"- Part 1 ({sw.Elapsed.TotalMilliseconds:F3}ms): {part1}");
+            if (!string.IsNullOrEmpty(part1))
+                PrintPart1(sw.Elapsed, part1);
 
-			sw.Restart();
-			string part2 = await ExecutePart2(challengeType);
-			sw.Stop();
+            sw.Restart();
+            string part2 = await ExecutePart2(challengeType);
+            sw.Stop();
 
-			if (!string.IsNullOrEmpty(part2))
-				Console.WriteLine($"- Part 2 ({sw.Elapsed.TotalMilliseconds:F3}ms): {part2}");
+            if (!string.IsNullOrEmpty(part2))
+                PrintPart2(sw.Elapsed, part2);
 
-			Console.WriteLine();
-		}
+            Console.WriteLine();
+        }
 
-		private async Task RunLatest()
-		{
-			var sw = new Stopwatch();
+        private async Task RunLatest()
+        {
+            var sw = new Stopwatch();
 
-			if (challengeTypeProvider.Count == 0)
-				return;
+            if (challengeTypeProvider.Count == 0)
+                return;
 
-			var pair= challengeTypeProvider.OrderByDescending(x => x.Key).First();
+            var pair = challengeTypeProvider.OrderByDescending(x => x.Key).First();
 
-			Console.WriteLine($"Day {pair.Key}:");
+            Console.WriteLine($"Day {pair.Key}:");
 
-			sw.Start();
-			string part1 = await ExecutePart1(pair.Value);
-			sw.Stop();
+            sw.Start();
+            string part1 = await ExecutePart1(pair.Value);
+            sw.Stop();
 
-			if (!string.IsNullOrEmpty(part1))
-				Console.WriteLine($"- Part 1 ({sw.Elapsed.TotalSeconds:F3}ms): {part1}");
+            if (!string.IsNullOrEmpty(part1))
+                PrintPart1(sw.Elapsed, part1);
 
-			sw.Restart();
-			string part2 = await ExecutePart2(pair.Value);
-			sw.Stop();
+            sw.Restart();
+            string part2 = await ExecutePart2(pair.Value);
+            sw.Stop();
 
-			if (!string.IsNullOrEmpty(part2))
-				Console.WriteLine($"- Part 2 ({sw.Elapsed.TotalSeconds:F3}ms): {part2}");
+            if (!string.IsNullOrEmpty(part2))
+                PrintPart2(sw.Elapsed, part2);
 
-			Console.WriteLine();
-		}
+            Console.WriteLine();
+        }
 
-		private async Task RunAll()
-		{
-			Console.WriteLine("Running all challenges...");
-			Console.WriteLine();
-			foreach (var pair in challengeTypeProvider.OrderBy(x => x.Key))
-			{
-				var sw = new Stopwatch();
-				Console.WriteLine($"Day {pair.Key}:");
+        private async Task RunAll()
+        {
+            Console.WriteLine("Running all challenges...");
+            Console.WriteLine();
+            foreach (var pair in challengeTypeProvider.OrderBy(x => x.Key))
+            {
+                var sw = new Stopwatch();
+                Console.WriteLine($"Day {pair.Key}:");
 
-				sw.Start();
-				string part1 = await ExecutePart1(pair.Value);
-				sw.Stop();
+                sw.Start();
+                string part1 = await ExecutePart1(pair.Value);
+                sw.Stop();
 
-				if (!string.IsNullOrEmpty(part1))
-					Console.WriteLine($"- Part 1 ({sw.Elapsed.TotalSeconds:F3}ms): {part1}");
+                if (!string.IsNullOrEmpty(part1))
+                    PrintPart1(sw.Elapsed, part1);
 
-				sw.Restart();
-				string part2 = await ExecutePart2(pair.Value);
-				sw.Stop();
+                sw.Restart();
+                string part2 = await ExecutePart2(pair.Value);
+                sw.Stop();
 
-				if (!string.IsNullOrEmpty(part2))
-					Console.WriteLine($"- Part 2 ({sw.Elapsed.TotalSeconds:F3}ms): {part2}");
+                if (!string.IsNullOrEmpty(part2))
+                    PrintPart2(sw.Elapsed, part2);
 
-				Console.WriteLine();
-			}
-		}
+                Console.WriteLine();
+            }
+        }
 
-		private async Task<string> ExecutePart1(Type type)
-		{
-			object challenge = serviceProvider.GetRequiredService(type);
-			await ChallengeHelper.SetupAsync(challenge);
-			return await ChallengeHelper.Part1Async(challenge);
-		}
+        private async Task<string> ExecutePart1(Type type)
+        {
+            object challenge = serviceProvider.GetRequiredService(type);
+            await ChallengeHelper.SetupAsync(challenge);
+            return await ChallengeHelper.Part1Async(challenge);
+        }
 
-		private async Task<string> ExecutePart2(Type type)
-		{
-			object challenge = serviceProvider.GetRequiredService(type);
-			await ChallengeHelper.SetupAsync(challenge);
-			return await ChallengeHelper.Part2Async(challenge);
-		}
-	}
+        private async Task<string> ExecutePart2(Type type)
+        {
+            object challenge = serviceProvider.GetRequiredService(type);
+            await ChallengeHelper.SetupAsync(challenge);
+            return await ChallengeHelper.Part2Async(challenge);
+        }
+
+        private void PrintPart1(TimeSpan timeTaken, string result) => PrintResult("Part 1", timeTaken, result);
+
+        private void PrintPart2(TimeSpan timeTaken, string result) => PrintResult("Part 2", timeTaken, result);
+
+        private void PrintResult(string part, TimeSpan timeTaken, string result)
+        {
+            Console.WriteLine($"- {part} ({timeTaken.TotalMilliseconds:F3}ms): {result}");
+        }
+    }
 }
