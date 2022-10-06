4c1c9a9f65b2d1132b260e546403c99b25b1c2bc
Yorrick Spierings <yi.spierings@gmail.com>
2022-10-06 13:14:55 +0200

    Day 15 of 2015 finished

---
+++ csharp/AdventOfCode.2015/Challenges/Challenge15.cs
@@ -0,0 +1,74 @@
+﻿using System.Text.RegularExpressions;
+using AdventOfCode.Core;
+using AdventOfCode.Core.IO;
+using AdventOfCode.Lib;
+
+namespace AdventOfCode2015.Challenges;
+
+[Challenge(15)]
+public class Challenge15
+{
+    private static readonly Regex Pattern =
+        new(@"\w+: capacity (-?\d+), durability (-?\d+), flavor (-?\d+), texture (-?\d+), calories (-?\d+)");
+    private readonly IInputReader _inputReader;
+
+    public Challenge15(IInputReader inputReader)
+    {
+        _inputReader = inputReader;
+    }
+
+    [Part1]
+    public async Task<string?> Part1Async()
+    {
+        var list = await _inputReader.ParseLinesAsync(15, ParseLine).ToListAsync();
+
+        var score = 0;
+        foreach (var distribution in EnumerableEx.Partition(100, list.Count))
+        {
+            var zip = list.Zip(distribution).ToList();
+            var cap = Math.Max(0, zip.Sum(x => x.First.Capacity * x.Second));
+            var dur = Math.Max(0,zip.Sum(x => x.First.Durability * x.Second));
+            var fla = Math.Max(0,zip.Sum(x => x.First.Flavor * x.Second));
+            var tex = Math.Max(0,zip.Sum(x => x.First.Texture * x.Second));
+            score = Math.Max(score, cap * dur * fla * tex);
+        }
+        
+        return score.ToString();
+    }
+    
+    [Part2]
+    public async Task<string?> Part2Async()
+    {
+        var list = await _inputReader.ParseLinesAsync(15, ParseLine).ToListAsync();
+
+        var score = 0;
+        foreach (var distribution in EnumerableEx.Partition(100, list.Count))
+        {
+            var zip = list.Zip(distribution).ToList();
+            var cap = Math.Max(0, zip.Sum(x => x.First.Capacity * x.Second));
+            var dur = Math.Max(0,zip.Sum(x => x.First.Durability * x.Second));
+            var fla = Math.Max(0,zip.Sum(x => x.First.Flavor * x.Second));
+            var tex = Math.Max(0,zip.Sum(x => x.First.Texture * x.Second));
+            var cal = Math.Max(0, zip.Sum(x => x.First.Calories * x.Second));
+            
+            if(cal == 500)
+                score = Math.Max(score, cap * dur * fla * tex);
+        }
+        
+        return score.ToString();
+    }
+
+    private static Ingredient ParseLine(string line)
+    {
+        var match = Pattern.Match(line);
+
+        var cap = int.Parse(match.Groups[1].Value);
+        var dur = int.Parse(match.Groups[2].Value);
+        var fla = int.Parse(match.Groups[3].Value);
+        var tex = int.Parse(match.Groups[4].Value);
+        var cal = int.Parse(match.Groups[5].Value);
+        return new Ingredient(cap, dur, fla, tex, cal);
+    }
+
+    private record Ingredient(int Capacity, int Durability, int Flavor, int Texture, int Calories);
+}
---
+++ csharp/AdventOfCode.Lib/EnumerableEx.cs
@@ -0,0 +1,80 @@
+﻿namespace AdventOfCode.Lib;
+
+public static class EnumerableEx
+{
+    /// <summary>
+    /// Enumerate all possible combinations of k items from a set of n
+    /// </summary>
+    /// <param name="n">Amount of items</param>
+    /// <param name="k">Amount to choose</param>
+    /// <returns></returns>
+    public static IEnumerable<int[]> Combinations(int n, int k)
+    {
+        var result = new int[k];
+        var stack = new Stack<int>(k);
+        stack.Push(0);
+
+        while (stack.Count > 0)
+        {
+            var index = stack.Count - 1;
+            var value = stack.Pop();
+
+            while (value < n)
+            {
+                result[index++] = value++;
+                stack.Push(value);
+
+                if (index != k) continue;
+                
+                yield return (int[])result.Clone();
+                break;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Partition number n from 0 to n into an array of size k
+    /// </summary>
+    /// <param name="n">Amount of items to distribute</param>
+    /// <param name="k">Amount of containers to distribute over</param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static IEnumerable<int[]> Partition(int n, int k)
+    {
+        if (n < k)
+            throw new ArgumentException("Value must be greater or equal to k", nameof(n));
+            
+        if (k < 1)
+            throw new ArgumentException("Value must be greater or equal to 1", nameof(k));
+
+        if (k == 1)
+        {
+            yield return new[] { n };
+            yield break;
+        }
+
+        var distribution = new int[k];
+
+        while (true)
+        {
+            distribution[0] = n - distribution.Skip(1).Sum();
+            
+            yield return (int[])distribution.Clone();
+
+            distribution[1]++;
+            distribution[0] = 0;
+
+            for (var i = 1; i < k-1; i++)
+            {
+                if (distribution.Sum() > n)
+                {
+                    distribution[i + 1]++;
+                    distribution[i] = 0;
+                }
+            }
+
+            if (distribution.Sum() > n)
+                yield break;
+        }
+    }
+}
