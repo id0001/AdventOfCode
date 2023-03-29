eeded58a9a410b1e30dc2ecc4ed4db6a451642ef
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2023-03-29 14:54:51 +0200

    2022 day 10 is done

---
+++ csharp/AdventOfCode.2022/Challenges/Challenge10.cs
@@ -0,0 +1,100 @@
+﻿using AdventOfCode.Core;
+using AdventOfCode.Core.IO;
+using System.Text;
+
+namespace AdventOfCode2022.Challenges
+{
+    [Challenge(10)]
+    public class Challenge10
+    {
+        private readonly IInputReader _inputReader;
+
+        public Challenge10(IInputReader inputReader)
+        {
+            _inputReader = inputReader;
+        }
+
+        [Part1]
+        public async Task<string> Part1Async()
+        {
+            int cycle = 0;
+            int x = 1;
+
+            int totalSum = 0;
+            var check = new HashSet<int> { 20, 60, 100, 140, 180, 220 };
+
+            await foreach (var (opcode, value) in _inputReader.ParseLinesAsync(10, ParseLine))
+            {
+                Cycle1(ref cycle, x, ref totalSum, check);
+
+                if (opcode == "addx")
+                {
+                    Cycle1(ref cycle, x, ref totalSum, check);
+
+                    x += value;
+                }
+            }
+
+            return totalSum.ToString();
+        }
+
+        [Part2]
+        public async Task<string> Part2Async()
+        {
+            int cycle = 0;
+            int r = 1;
+
+            var crt = new char[40 * 6];
+
+            await foreach (var (opcode, value) in _inputReader.ParseLinesAsync(10, ParseLine))
+            {
+                Cycle2(ref cycle, r, crt);
+
+                if (opcode == "addx")
+                {
+                    Cycle2(ref cycle, r, crt);
+
+                    r += value;
+                }
+            }
+
+            var sb = new StringBuilder();
+            sb.AppendLine();
+            for (int y = 0; y < 6; y++)
+            {
+                for (int x = 0; x < 40; x++)
+                    sb.Append(crt[y * 40 + x]);
+
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
+        }
+
+        private static void Cycle1(ref int cycle, int x, ref int totalSum, HashSet<int> check)
+        {
+            cycle++;
+
+            if (check.Contains(cycle))
+                totalSum += x * cycle;
+        }
+
+        private static void Cycle2(ref int cycle, int r, char[] crt)
+        {
+            cycle++;
+
+            int pos = (cycle - 1) % 40;
+
+            if (pos >= r - 1 && pos <= r + 1)
+                crt[cycle - 1] = '#';
+            else
+                crt[cycle - 1] = '.';
+        }
+
+        private (string, int) ParseLine(string line)
+        {
+            string[] split = line.Split(' ');
+            return (split[0], split.Length > 1 ? int.Parse(split[1]) : 0);
+        }
+    }
+}
