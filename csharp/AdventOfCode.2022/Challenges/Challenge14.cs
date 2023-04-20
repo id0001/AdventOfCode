ae808c3a120b520072f1361ec77c9eebd0528aff
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2023-04-20 09:19:13 +0200

    2022 day 14 is done

---
+++ csharp/AdventOfCode.2022/Challenges/Challenge14.cs
@@ -0,0 +1,189 @@
+﻿using AdventOfCode.Core;
+using AdventOfCode.Core.IO;
+using AdventOfCode.Lib;
+using AdventOfCode.Lib.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Challenges
+{
+    [Challenge(14)]
+    public class Challenge14
+    {
+        private readonly IInputReader _inputReader;
+
+        public Challenge14(IInputReader inputReader)
+        {
+            _inputReader = inputReader;
+        }
+
+        [Part1]
+        public async Task<string> Part1Async()
+        {
+            var map = new SparseSpatialMap<Point2, char>();
+
+            await foreach (var points in _inputReader.ParseLinesAsync(14, ParseLine))
+            {
+                var iterator = points.GetEnumerator();
+                foreach (var (current, next) in points.CurrentAndNext())
+                {
+                    foreach (var p in Point2.BresenhamLine(current, next))
+                    {
+                        map.AddOrUpdate(p, '#');
+                    }
+                }
+            }
+
+            var start = new Point2(500, 0);
+            int s = 0;
+            int bottomBound = map.Bounds.GetMax(1);
+            while (true)
+            {
+                map[start] = 'o';
+
+                while (true)
+                {
+                    var bottom = start + new Point2(0, 1);
+                    var bottomLeft = start + new Point2(-1, 1);
+                    var bottomRight = start + new Point2(1, 1);
+
+                    if (bottom.Y > bottomBound)
+                    {
+                        PrintMap(map);
+                        return s.ToString();
+                    }
+
+                    if (map.GetValueOrDefault(bottom, '.') == '.')
+                    {
+                        map.AddOrUpdate(start, '.');
+                        map.AddOrUpdate(bottom, 'o');
+                        start = bottom;
+                    }
+                    else if (map.GetValueOrDefault(bottomLeft, '.') == '.')
+                    {
+                        map.AddOrUpdate(start, '.');
+                        map.AddOrUpdate(bottomLeft, 'o');
+                        start = bottomLeft;
+                    }
+                    else if (map.GetValueOrDefault(bottomRight, '.') == '.')
+                    {
+                        map.AddOrUpdate(start, '.');
+                        map.AddOrUpdate(bottomRight, 'o');
+                        start = bottomRight;
+                    }
+                    else
+                    {
+                        start = new Point2(500, 0);
+                        s++;
+                        break;
+                    }
+                }
+            }
+        }
+
+        [Part2]
+        public async Task<string> Part2Async()
+        {
+            var map = new SparseSpatialMap<Point2, char>();
+
+            await foreach (var points in _inputReader.ParseLinesAsync(14, ParseLine))
+            {
+                var iterator = points.GetEnumerator();
+                foreach (var (current, next) in points.CurrentAndNext())
+                {
+                    foreach (var p in Point2.BresenhamLine(current, next))
+                    {
+                        map.AddOrUpdate(p, '#');
+                    }
+                }
+            }
+
+            var start = new Point2(500, 0);
+            int s = 0;
+            int bottomBound = map.Bounds.GetMax(1) + 1;
+            while (true)
+            {
+                map[start] = 'o';
+
+                while (true)
+                {
+                    var bottom = start + new Point2(0, 1);
+                    var bottomLeft = start + new Point2(-1, 1);
+                    var bottomRight = start + new Point2(1, 1);
+
+                    if(bottom.Y == bottomBound)
+                    {
+                        start = new Point2(500, 0);
+                        s++;
+                        break;
+                    }
+                    else if (map.GetValueOrDefault(bottom, '.') == '.')
+                    {
+                        map.AddOrUpdate(start, '.');
+                        map.AddOrUpdate(bottom, 'o');
+                        start = bottom;
+                    }
+                    else if (map.GetValueOrDefault(bottomLeft, '.') == '.')
+                    {
+                        map.AddOrUpdate(start, '.');
+                        map.AddOrUpdate(bottomLeft, 'o');
+                        start = bottomLeft;
+                    }
+                    else if (map.GetValueOrDefault(bottomRight, '.') == '.')
+                    {
+                        map.AddOrUpdate(start, '.');
+                        map.AddOrUpdate(bottomRight, 'o');
+                        start = bottomRight;
+                    }
+                    else if(start == new Point2(500, 0))
+                    {
+                        PrintMap(map);
+                        s++;
+                        return s.ToString();
+                    }
+                    else
+                    {
+                        start = new Point2(500, 0);
+                        s++;
+                        break;
+                    }
+                }
+            }
+        }
+
+        private static IEnumerable<Point2> ParseLine(string line)
+        {
+            string[] split = line.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+            foreach (var part in split)
+            {
+                var arr = part.Split(',').Select(int.Parse).ToArray();
+                yield return new Point2(arr[0], arr[1]);
+            }
+        }
+
+        private static void PrintMap(SparseSpatialMap<Point2, char> map)
+        {
+            var sb = new StringBuilder(); ;
+
+            for (int y = map.Bounds.GetMin(1); y <= map.Bounds.GetMax(1); y++)
+            {
+                for (int x = map.Bounds.GetMin(0); x <= map.Bounds.GetMax(0); x++)
+                {
+                    var p = new Point2(x, y);
+                    if (map.ContainsKey(p))
+                        sb.Append(map[p]);
+                    else
+                        sb.Append('.');
+                }
+
+                sb.AppendLine();
+            }
+
+            File.WriteAllText(@"C:\Temp\output.txt", sb.ToString());
+        }
+    }
+}
--- csharp/AdventOfCode.Lib/Point2.cs
+++ csharp/AdventOfCode.Lib/Point2.cs
@@ -24,16 +24,16 @@ public readonly struct Point2 : IPoint, IEquatable<Point2>
     public IEnumerable<Point2> GetNeighbors(bool includeDiagonal = false)
     {
         for (var y = -1; y <= 1; y++)
-        for (var x = -1; x <= 1; x++)
-        {
-            if (!includeDiagonal && !((x == 0) ^ (y == 0)))
-                continue;
-    
-            if (x == 0 && y == 0)
-                continue;
-    
-            yield return new Point2(X + x, Y + y);
-        }
+            for (var x = -1; x <= 1; x++)
+            {
+                if (!includeDiagonal && !((x == 0) ^ (y == 0)))
+                    continue;
+
+                if (x == 0 && y == 0)
+                    continue;
+
+                yield return new Point2(X + x, Y + y);
+            }
     }
 
     public void Deconstruct(out int x, out int y) => (x, y) = (X, Y);
@@ -66,7 +66,7 @@ public readonly struct Point2 : IPoint, IEquatable<Point2>
     public static Point2 Add(Point2 left, Point2 right) => new(left.X + right.X, left.Y + right.Y);
 
     public static Point2 Multiply(Point2 point, int multiplier) => new(point.X * multiplier, point.Y * multiplier);
-    
+
     public static Point2 Multiply(Point2 left, Point2 right) => new(left.X * right.X, left.Y * right.Y);
 
     public static int DistanceSquared(Point2 left, Point2 right)
@@ -77,7 +77,7 @@ public readonly struct Point2 : IPoint, IEquatable<Point2>
     }
 
     public static double Distance(Point2 left, Point2 right) => System.Math.Sqrt(DistanceSquared(left, right));
-    
+
     /// <summary>
     /// Rotates a point around a pivot on a circle by the amount defined by angle
     /// </summary>
@@ -96,17 +96,95 @@ public readonly struct Point2 : IPoint, IEquatable<Point2>
         return new Point2(x, y);
     }
 
+    public static IEnumerable<Point2> BresenhamLine(Point2 from, Point2 to)
+    {
+        if (System.Math.Abs(to.Y - from.Y) < System.Math.Abs(to.X - from.X))
+        {
+            if (from.X > to.X)
+                return BresenhamLineLow(to.X, to.Y, from.X, from.Y).Reverse();
+            else
+                return BresenhamLineLow(from.X, from.Y, to.X, to.Y);
+        }
+        else
+        {
+            if (from.Y > to.Y)
+                return BresenhamLineHigh(to.X, to.Y, from.X, from.Y).Reverse();
+            else
+                return BresenhamLineHigh(from.X, from.Y, to.X, to.Y);
+        }
+    }
+
     public static bool operator ==(Point2 left, Point2 right) => left.Equals(right);
 
     public static bool operator !=(Point2 left, Point2 right) => !(left == right);
 
     public static Point2 operator +(Point2 left, Point2 right) => Add(left, right);
-    
+
     public static Point2 operator -(Point2 left, Point2 right) => Subtract(left, right);
 
     public static Point2 operator *(Point2 left, int multiplier) => Multiply(left, multiplier);
-    
+
     public static Point2 operator *(Point2 left, Point2 right) => Multiply(left, right);
 
     public static implicit operator Vector2(Point2 value) => new Vector2(value.X, value.Y);
+
+    private static IEnumerable<Point2> BresenhamLineHigh(int x0, int y0, int x1, int y1)
+    {
+        int dx = x1 - x0;
+        int dy = y1 - y0;
+        int xi = 1;
+        if (dx < 0)
+        {
+            xi = -1;
+            dx = -dx;
+        }
+
+        int d = (2 * dx) - dy;
+        int x = x0;
+
+
+
+        for (int y = y0; y <= y1; y++)
+        {
+            yield return new Point2(x, y);
+            if (d > 0)
+            {
+                x = x + xi;
+                d = d + (2 * (dx - dy));
+            }
+            else
+            {
+                d = d + 2 * dx;
+            }
+        }
+    }
+
+    private static IEnumerable<Point2> BresenhamLineLow(int x0, int y0, int x1, int y1)
+    {
+        int dx = x1 - x0;
+        int dy = y1 - y0;
+        int yi = 1;
+        if (dy < 0)
+        {
+            yi = -1;
+            dy = -dy;
+        }
+
+        int d = (2 * dy) - dx;
+        int y = y0;
+
+        for (int x = x0; x <= x1; x++)
+        {
+            yield return new Point2(x, y);
+            if (d > 0)
+            {
+                y = y + yi;
+                d = d + (2 * (dy - dx));
+            }
+            else
+            {
+                d = d + 2 * dy;
+            }
+        }
+    }
 }
