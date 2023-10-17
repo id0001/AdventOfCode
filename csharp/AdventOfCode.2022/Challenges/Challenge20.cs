f67acdb8bcbad4d597ecaa7d2470ea953aabf750
= <yi.spierings@gmail.com>
2023-10-17 21:21:24 +0200

    Day 21 part 1 is done. WIP part 2

---
+++ csharp/AdventOfCode.2022/Challenges/Challenge20.cs
@@ -0,0 +1,78 @@
+﻿using AdventOfCode.Core;
+using AdventOfCode.Core.IO;
+using AdventOfCode.Lib.Collections;
+using AdventOfCode.Lib.Math;
+
+namespace AdventOfCode2022.Challenges;
+
+[Challenge(20)]
+public class Challenge20
+{
+    private readonly IInputReader _inputReader;
+
+    public Challenge20(IInputReader inputReader)
+    {
+        _inputReader = inputReader;
+    }
+
+    [Part1]
+    public async Task<string> Part1Async()
+    {
+        var numbers = await _inputReader.ReadLinesAsync<int>(20).ToListAsync();
+        var indices = Enumerable.Range(0, numbers.Count).ToList();
+
+        for (var i = 0; i < numbers.Count; i++)
+        {
+            int idx = indices.IndexOf(i);
+            int num = numbers[idx];
+            numbers.RemoveAt(idx);
+            indices.RemoveAt(idx);
+            var newIndex = Euclid.Modulus(idx + num, numbers.Count);
+            if (newIndex == 0)
+            {
+                numbers.Add(num);
+                indices.Add(i);
+            }
+            else
+            {
+                numbers.Insert( newIndex, num);
+                indices.Insert(newIndex, i);
+            }
+        }
+
+        var idxOf0 = numbers.IndexOf(0);
+        return (numbers[Euclid.Modulus(idxOf0 + 1000, numbers.Count)] + numbers[Euclid.Modulus(idxOf0 + 2000, numbers.Count)] + numbers[Euclid.Modulus(idxOf0 + 3000, numbers.Count)]).ToString();
+    }
+    
+    [Part2]
+    public async Task<string> Part2Async()
+    {
+        var numbers = await _inputReader.ReadLinesAsync<int>(20).Select(x => x * 811589153L).ToListAsync();
+        var indices = Enumerable.Range(0, numbers.Count).ToList();
+
+        for (var j = 0; j < 10; j++)
+        {
+            for (var i = 0; i < numbers.Count; i++)
+            {
+                int idx = indices.IndexOf(i);
+                long num = numbers[idx];
+                numbers.RemoveAt(idx);
+                indices.RemoveAt(idx);
+                var newIndex = (int) Euclid.Modulus(idx + num, numbers.Count);
+                if (newIndex == 0)
+                {
+                    numbers.Add(num);
+                    indices.Add(i);
+                }
+                else
+                {
+                    numbers.Insert(newIndex, num);
+                    indices.Insert(newIndex, i);
+                }
+            }
+        }
+
+        var idxOf0 = numbers.IndexOf(0);
+        return (numbers[Euclid.Modulus(idxOf0 + 1000, numbers.Count)] + numbers[Euclid.Modulus(idxOf0 + 2000, numbers.Count)] + numbers[Euclid.Modulus(idxOf0 + 3000, numbers.Count)]).ToString();
+    }
+}
---
+++ csharp/AdventOfCode.2022/Challenges/Challenge21.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq.Expressions;
+using AdventOfCode.Core;
+using AdventOfCode.Core.IO;
+
+namespace AdventOfCode2022.Challenges;
+
+[Challenge(21)]
+public class Challenge21
+{
+    private readonly IInputReader _inputReader;
+
+    public Challenge21(IInputReader inputReader)
+    {
+        _inputReader = inputReader;
+    }
+
+    [Part1]
+    public async Task<string> Part1Async()
+    {
+        var lines = await _inputReader.ReadLinesAsync(21)
+            .Select(line => line.Split(':', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries))
+            .ToDictionaryAsync(kv => kv[0], kv => kv[1]);
+
+        var result = Expression.Lambda<Func<long>>(Parse(new Dictionary<string, Expression>(), lines, "root")).Compile();
+        return result().ToString();
+    }
+
+    private static Expression Parse(IDictionary<string, Expression> cache, IDictionary<string, string> map, string key)
+    {
+        if (cache.ContainsKey(key))
+            return cache[key];
+        
+        var line = map[key];
+        if (long.TryParse(line, out var num))
+        {
+            cache.Add(key, Expression.Constant(num));
+        }
+        else
+        {
+            var split = line.Split(' ');
+            cache.Add(key, split[1] switch
+            {
+                "+" => Expression.Add(Parse(cache, map, split[0]), Parse(cache, map, split[2])),
+                "-" => Expression.Subtract(Parse(cache, map, split[0]), Parse(cache, map, split[2])),
+                "*" => Expression.Multiply(Parse(cache, map, split[0]), Parse(cache, map, split[2])),
+                "/" => Expression.Divide(Parse(cache, map, split[0]), Parse(cache, map, split[2])),
+                _ => throw new NotImplementedException()
+            });
+        }
+        
+        return cache[key];
+    }
+}
