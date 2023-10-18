df7e3869e444bb03e486f2ff4193f6cf2d587b4f
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2023-10-18 16:56:19 +0200

    Added diagram for transformations

--- csharp/AdventOfCode.2022/Challenges/Challenge22.cs
+++ csharp/AdventOfCode.2022/Challenges/Challenge22.cs
@@ -75,6 +75,73 @@ namespace AdventOfCode2022.Challenges
             return (rows + cols + facing).ToString();
         }
 
+        [Part2]
+        public async Task<string> Part2Async()
+        {
+            var lines = await _inputReader.ReadLinesAsync(22).ToListAsync();
+            var (map, line) = ReadGridWithInstructions(lines);
+
+            var pos = FindLeftMostOpenTile(map);
+            var dir = new Point2(1, 0);
+            var instructions = new Queue<char>(line);
+            var numberBuilder = new StringBuilder();
+
+            for(int y = 0; y < map.GetLength(0); y++)
+            {
+                for(int x = 0; x < map.GetLength(1); x++)
+                {
+                    int zoneX = (int)Math.Floor(x / 50d);
+                    int zoneY = (int)Math.Floor(y / 50d);
+                }
+            }
+
+            foreach (var instruction in instructions)
+            {
+                if (char.IsNumber(instruction))
+                    numberBuilder.Append(instruction);
+                else
+                {
+                    if (numberBuilder.Length > 0)
+                    {
+                        int num = int.Parse(numberBuilder.ToString());
+                        numberBuilder.Clear();
+
+                        for (int i = 0; i < num; i++)
+                        {
+                            var next = GetNext(map, pos, dir);
+                            if (map[next.Y, next.X] == '.')
+                                pos = next;
+                        }
+                    }
+
+                    dir = instruction switch
+                    {
+                        'L' => Point2.Turn(dir, Point2.Zero, -Trigonometry.DegreeToRadian(90)),
+                        'R' => Point2.Turn(dir, Point2.Zero, Trigonometry.DegreeToRadian(90)),
+                        _ => throw new NotImplementedException()
+                    };
+                }
+            }
+
+            if (numberBuilder.Length > 0)
+            {
+                int num = int.Parse(numberBuilder.ToString());
+                numberBuilder.Clear();
+
+                for (int i = 0; i < num; i++)
+                {
+                    var next = GetNext(map, pos, dir);
+                    if (map[next.Y, next.X] == '.')
+                        pos = next;
+                }
+            }
+
+            var facing = GetFacing(dir);
+            var rows = (pos.Y + 1) * 1000;
+            var cols = (pos.X + 1) * 4;
+            return (rows + cols + facing).ToString();
+        }
+
         private static int GetFacing(Point2 direction)
         {
             return direction switch
@@ -123,6 +190,23 @@ namespace AdventOfCode2022.Challenges
 
         private static Point2 GetNext2(char[,] map, Point2 position, Point2 direction)
         {
+            /*    |    x0     |    x1   |     y0    |    y1     |
+             * ---+-----------+---------+-----------+-----------+
+             * x0 |###########| (x+l,y) | (y,x)     | (l-y,l-x) |
+             * ---+-----------+---------+-----------+-----------+
+             * x1 | (x-l,y)   | (x,l-y) | (l-y,l-x) |########## |
+             * ---+-----------+---------+-----------+-----------+
+             * y0 | (y,x)     | (l,h-x) |###########| (l-x,y)   |
+             * ---+-----------+---------+-----------+-----------+
+             * y1 | (l-y,l-x) |#########| (l-x,y)   | (l-x,y)   |
+             * ---+-----------+---------+-----------+-----------+
+             * 
+             */
+
+
+
+
+
             var next = new Point2(Euclid.Modulus(position.X + direction.X, map.GetLength(1)), Euclid.Modulus(position.Y + direction.Y, map.GetLength(0)));
             if (map[next.Y, next.X] == ' ')
             {
