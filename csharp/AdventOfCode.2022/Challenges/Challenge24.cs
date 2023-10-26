593e927fe4eb0960cc74180e4170f7e8527f7e85
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2023-10-26 12:39:38 +0200

    Day 24 is finished

---
+++ csharp/AdventOfCode.2022/Challenges/Challenge24.cs
@@ -0,0 +1,122 @@
+using AdventOfCode.Core;
+using AdventOfCode.Core.IO;
+using AdventOfCode.Lib;
+using AdventOfCode.Lib.Math;
+using AdventOfCode.Lib.PathFinding;
+using System.Collections;
+using System.Data;
+using System.Diagnostics;
+using System.Text;
+
+namespace AdventOfCode2022.Challenges;
+
+[Challenge(24)]
+public class Challenge24
+{
+    private readonly IInputReader _inputReader;
+
+    public Challenge24(IInputReader inputReader)
+    {
+        _inputReader = inputReader;
+    }
+
+    [Part1]
+    public async Task<string> Part1Async()
+    {
+        var grid = await _inputReader.ReadGridAsync(24);
+
+        Point2 start = new Point2(1, 0);
+        Point2 goal = new Point2(120, 26);
+
+        var state = new State(start, 0);
+
+        var weight = new Func<State, State, int>((c, n) => 1);
+        var heuristic = new Func<State, int>(c => Point2.ManhattanDistance(c.Position, goal));
+        var neighbors = new Func<State, IEnumerable<State>>(c => GetAdjecent(grid, c));
+        var goalReached = new Func<State, bool>(c => c.Position == goal);
+
+        var astar = new AStar<State>(neighbors, weight, heuristic);
+        astar.TryPath(state, goalReached, out var path, out var minutes);
+
+        return minutes.ToString();
+    }
+
+
+    [Part2]
+    public async Task<string> Part2Async()
+    {
+        var grid = await _inputReader.ReadGridAsync(24);
+
+        Point2 start = new Point2(1, 0);
+        Point2 goal = new Point2(120, 26);
+
+        var state = new State2(start, 0, 0);
+
+        var weight = new Func<State2, State2, int>((c, n) => 1);
+        var heuristic = new Func<State2, int>(c =>
+        {
+            if (c.Phase == 1)
+                return Point2.ManhattanDistance(c.Position, start);
+
+            return Point2.ManhattanDistance(c.Position, goal);
+        });
+
+        var neighbors = new Func<State2, IEnumerable<State2>>(c => GetAdjecent2(grid, start, goal, c));
+        var goalReached = new Func<State2, bool>(c => c.Position == goal && c.Phase == 2);
+
+        var astar = new AStar<State2>(neighbors, weight, heuristic);
+        astar.TryPath(state, goalReached, out var path, out var minutes);
+
+        return minutes.ToString();
+    }
+
+    private static IEnumerable<State> GetAdjecent(char[,] grid, State current)
+    {
+        foreach (var neighbor in current.Position.GetNeighbors())
+        {
+            if (neighbor.X < 0 || neighbor.X >= grid.GetLength(1) || neighbor.Y < 0 || neighbor.Y >= grid.GetLength(0) || grid[neighbor.Y, neighbor.X] == '#')
+                continue;
+
+            if (!HasBlizzard(grid, neighbor, current.Time + 1))
+                yield return new State(neighbor, current.Time + 1);
+        }
+
+        if (!HasBlizzard(grid, current.Position, current.Time + 1))
+            yield return new State(current.Position, current.Time + 1);
+    }
+
+    private static IEnumerable<State2> GetAdjecent2(char[,] grid, Point2 start, Point2 goal, State2 current)
+    {
+        var nextPhase = current.Phase;
+        if (current.Position == goal && current.Phase == 0)
+            nextPhase = 1;
+        else if (current.Position == start && current.Phase == 1)
+            nextPhase = 2;
+
+        foreach (var neighbor in current.Position.GetNeighbors())
+        {
+            if (neighbor.X < 0 || neighbor.X >= grid.GetLength(1) || neighbor.Y < 0 || neighbor.Y >= grid.GetLength(0) || grid[neighbor.Y, neighbor.X] == '#')
+                continue;
+
+            if (!HasBlizzard(grid, neighbor, current.Time + 1))
+                yield return new State2(neighbor, nextPhase, current.Time + 1);
+        }
+
+        if (!HasBlizzard(grid, current.Position, current.Time + 1))
+            yield return new State2(current.Position, nextPhase, current.Time + 1);
+    }
+
+    private static bool HasBlizzard(char[,] grid, Point2 p, int time)
+    {
+        return grid[Wrap(p.Y - time, grid.GetLength(0)), p.X] == 'v'
+            || grid[Wrap(p.Y + time, grid.GetLength(0)), p.X] == '^'
+            || grid[p.Y, Wrap(p.X - time, grid.GetLength(1))] == '>'
+            || grid[p.Y, Wrap(p.X + time, grid.GetLength(1))] == '<';
+    }
+
+    private static int Wrap(int v, int max) => Euclid.Modulus(v - 1, max - 2) + 1;
+
+    public record State(Point2 Position, int Time);
+
+    public record State2(Point2 Position, int Phase, int Time);
+}
--- csharp/AdventOfCode.Lib/PathFinding/AStar.cs
+++ csharp/AdventOfCode.Lib/PathFinding/AStar.cs
@@ -2,27 +2,42 @@
 
 public class AStar<T> where T : notnull
 {
-    private readonly Func<T, IEnumerable<T>> _getAdjacent;
+    private readonly Func<T, IEnumerable<T>> _adjacent;
     private readonly Func<T, T, int> _weight;
+    private readonly Func<T, int> _heuristic;
 
-    public AStar(Func<T, IEnumerable<T>> getAdjacent, Func<T, T, int> weight)
+    public AStar(Func<T, IEnumerable<T>> adjecent, Func<T, T, int> weight) : this(adjecent, weight, _ => 0)
     {
-        _getAdjacent = getAdjacent;
-        _weight = weight;
     }
 
-    public Func<T, int> Heuristic { get; } = _ => 0;
+    public AStar(Func<T, IEnumerable<T>> adjecent, Func<T, T, int> weight, Func<T, int> heuristic)
+    {
+        _adjacent = adjecent;
+        _weight = weight;
+        _heuristic = heuristic;
+    }
 
-    public bool TryPath(T start, Func<T, bool> isFinished, out IEnumerable<T> path,
-        out int totalDistance)
-        => TryPath(start, _getAdjacent, _weight, Heuristic, isFinished, out path, out totalDistance);
+    public bool TryPath(
+        T start,
+        Func<T, bool> isFinished,
+        out IEnumerable<T> path,
+        out int totalCost
+        )
+        => TryPath(start, _adjacent, _weight, _heuristic, isFinished, out path, out totalCost);
 
-    private static bool TryPath(T start, Func<T, IEnumerable<T>> getAdjacent, Func<T, T, int> weight,
-        Func<T, int> heuristic, Func<T, bool> isFinished, out IEnumerable<T> path, out int totalDistance)
+    private static bool TryPath(
+        T start,
+        Func<T, IEnumerable<T>> getAdjacent,
+        Func<T, T, int> weight,
+        Func<T, int> heuristic,
+        Func<T, bool> isFinished,
+        out IEnumerable<T> path,
+        out int totalCost
+        )
     {
         var queue = new PriorityQueue<T, int>();
-        var previous = new Dictionary<T, T>();
-        var distances = new Dictionary<T, int> { { start, 0 } };
+        var cameFrom = new Dictionary<T, T>();
+        var costSoFar = new Dictionary<T, int> { { start, 0 } };
 
         queue.Enqueue(start, 0);
 
@@ -32,31 +47,26 @@ public class AStar<T> where T : notnull
 
             if (isFinished(currentNode))
             {
-                path = GetPath(currentNode, previous);
-                totalDistance = distances[currentNode];
+                path = GetPath(currentNode, cameFrom);
+                totalCost = costSoFar[currentNode];
                 return true;
             }
 
             foreach (var adjacent in getAdjacent(currentNode))
             {
-                var newDistance = distances[currentNode] + weight(currentNode, adjacent);
-                if (!distances.TryGetValue(adjacent, out var oldDistance))
-                    oldDistance = int.MaxValue;
+                var newCost = costSoFar[currentNode] + weight(currentNode, adjacent);
 
-                if (newDistance >= oldDistance) continue;
-
-                if (!distances.TryAdd(adjacent, newDistance))
-                    distances[adjacent] = newDistance;
-
-                if (!previous.TryAdd(adjacent, currentNode))
-                    previous[adjacent] = currentNode;
-                
-                queue.Enqueue(adjacent, newDistance + heuristic(adjacent));
+                if (!costSoFar.ContainsKey(adjacent) || newCost < costSoFar[adjacent])
+                {
+                    costSoFar[adjacent] = newCost;
+                    queue.Enqueue(adjacent, newCost + heuristic(adjacent));
+                    cameFrom[adjacent] = currentNode;
+                }
             }
         }
 
         path = Enumerable.Empty<T>();
-        totalDistance = 0;
+        totalCost = 0;
         return false;
     }
 
