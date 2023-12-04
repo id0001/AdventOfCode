7b9427e246051bb4e0fcdcfc3491c393be078e53
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2023-12-04 07:07:38 +0100

    Day 4 is done

--- csharp/AdventOfCode.2023/Challenges/Challenge04.cs
+++ csharp/AdventOfCode.2023/Challenges/Challenge04.cs
@@ -1,9 +1,11 @@
 using AdventOfCode.Core;
+using AdventOfCode.Core.Extensions;
 using AdventOfCode.Core.IO;
+using AdventOfCode.Lib;
 
 namespace AdventOfCode2023.Challenges;
 
-//[Challenge(4)]
+[Challenge(4)]
 public class Challenge04
 {
     private readonly IInputReader _inputReader;
@@ -14,14 +16,59 @@ public class Challenge04
     }
 
     [Part1]
-    public async Task<string> Part1Async()
+    public async Task<string?> Part1Async()
     {
-        return string.Empty;
+        return await _inputReader.ParseLinesAsync(4, ParseLine)
+            .Select(card => card.Score)
+            .SumAsync()
+            .ToStringAsync();
     }
 
-    // [Part2]
+    [Part2]
     public async Task<string> Part2Async()
     {
-        return string.Empty;
+        var lookup = await _inputReader.ParseLinesAsync(4, ParseLine).ToDictionaryAsync(kv => kv.Id, kv => kv.AmountOfWinningNumbers);
+        return lookup.Keys.Sum(c => CountCards(c, lookup)).ToString();
+    }
+
+    private int CountCards(int id, Dictionary<int, int> lookup)
+    {
+        return 1 + Enumerable.Range(id + 1, lookup[id])
+            .Where(lookup.ContainsKey)
+            .Sum(c => CountCards(c, lookup));
+    }
+
+    private ScratchCard ParseLine(string line)
+    {
+        return line
+            .SplitBy(":")
+            .Select(x => new ScratchCard
+            {
+                Id = int.Parse(x.First().SplitBy(" ").Second()),
+                WinningNumbers = x.Second().SplitBy("|").First().SplitBy(" ").Values.Select(int.Parse).ToArray(),
+                Numbers = x.Second().SplitBy("|").Second().SplitBy(" ").Values.Select(int.Parse).ToArray(),
+            }); ;
+    }
+
+    private record ScratchCard
+    {
+        private int amountOfWinningNumbers = -1;
+
+        public required int Id { get; init; }
+        public required int[] WinningNumbers { get; init; }
+        public required int[] Numbers { get; init; }
+
+        public int AmountOfWinningNumbers => amountOfWinningNumbers < 0 ? amountOfWinningNumbers = Numbers.Intersect(WinningNumbers).Count() : amountOfWinningNumbers;
+
+        public int Score
+        {
+            get
+            {
+                if (AmountOfWinningNumbers == 0)
+                    return 0;
+
+                return (int)Math.Pow(2, AmountOfWinningNumbers - 1);
+            }
+        }
     }
 }
