9f7056eb45c8d8be67ccbed78b6d94fe40d2e827
= <yi.spierings@gmail.com>
2023-12-06 06:31:08 +0100

    Day 6 is done

--- csharp/AdventOfCode.2023/Challenges/Challenge06.cs
+++ csharp/AdventOfCode.2023/Challenges/Challenge06.cs
@@ -1,13 +1,14 @@
 using AdventOfCode.Core;
 using AdventOfCode.Core.IO;
+using AdventOfCode.Lib;
 
 namespace AdventOfCode2023.Challenges;
 
-//[Challenge(6)]
+[Challenge(6)]
 public class Challenge06
 {
     private readonly IInputReader _inputReader;
-
+    
     public Challenge06(IInputReader inputReader)
     {
         _inputReader = inputReader;
@@ -16,12 +17,47 @@ public class Challenge06
     [Part1]
     public async Task<string> Part1Async()
     {
-        return string.Empty;
+        var (time, distance) = ParseInput(await _inputReader.ReadAllTextAsync(6));
+        return time.Zip(distance).Select(zip => CountWinningDistances(zip.First, zip.Second)).Product().ToString();
     }
 
-    // [Part2]
+    [Part2]
     public async Task<string> Part2Async()
     {
-        return string.Empty;
+        var (time, distance) = ParseInput2(await _inputReader.ReadAllTextAsync(6));
+        return CountWinningDistances(time, distance).ToString();
+    }
+
+    private long CountWinningDistances(long time, long distance)
+    {
+        int c = 0;
+        for (long i = 0; i <= time; i++)
+        {
+            var d = i * (time - i);
+            if (d > distance)
+                c++;
+        }
+
+        return c;
+    }
+
+    private (long[], long[]) ParseInput(string input)
+    {
+        var nl = Environment.NewLine;
+        return input.SplitBy(nl, split => (
+                split.First.SplitBy(":").Second.SplitBy(" ").Select(long.Parse).ToArray(),
+                split.Second.SplitBy(":").Second.SplitBy(" ").Select(long.Parse).ToArray()
+            )
+        );
+    }
+    
+    private (long, long) ParseInput2(string input)
+    {
+        var nl = Environment.NewLine;
+        return input.SplitBy(nl, split => (
+                int.Parse(split.First.SplitBy(":").Second.Replace(" ", "")),
+                long.Parse(split.Second.SplitBy(":").Second.Replace(" ", ""))
+            )
+        );
     }
 }
