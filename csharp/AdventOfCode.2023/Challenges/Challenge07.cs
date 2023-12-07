736b05b344e51f829ff22c91182392876b184986
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2023-12-07 09:21:08 +0100

    Cleaned up code

--- csharp/AdventOfCode.2023/Challenges/Challenge07.cs
+++ csharp/AdventOfCode.2023/Challenges/Challenge07.cs
@@ -7,40 +7,6 @@ namespace AdventOfCode2023.Challenges;
 [Challenge(7)]
 public class Challenge07
 {
-    private static readonly Dictionary<char, int> CardStrength = new()
-    {
-        {'2', 2},
-        {'3', 3},
-        {'4', 4},
-        {'5', 5},
-        {'6', 6},
-        {'7', 7},
-        {'8', 8},
-        {'9', 9},
-        {'T', 10},
-        {'J', 11},
-        {'Q', 12},
-        {'K', 13},
-        {'A', 14}
-    };
-
-    private static readonly Dictionary<char, int> CardStrength2 = new()
-    {
-        {'J', 1},
-        {'2', 2},
-        {'3', 3},
-        {'4', 4},
-        {'5', 5},
-        {'6', 6},
-        {'7', 7},
-        {'8', 8},
-        {'9', 9},
-        {'T', 10},
-        {'Q', 12},
-        {'K', 13},
-        {'A', 14}
-    };
-
     private readonly IInputReader _inputReader;
 
     public Challenge07(IInputReader inputReader)
@@ -49,112 +15,78 @@ public class Challenge07
     }
 
     [Part1]
-    public async Task<string?> Part1Async()
-    {
-        return await _inputReader.ParseLinesAsync(7, ParseLine)
-            .OrderBy(x => CardStrength[x.Cards[4]])
-            .OrderBy(x => CardStrength[x.Cards[3]])
-            .OrderBy(x => CardStrength[x.Cards[2]])
-            .OrderBy(x => CardStrength[x.Cards[1]])
-            .OrderBy(x => CardStrength[x.Cards[0]])
-            .OrderBy(x => Score(x.Cards))
+    public async Task<string?> Part1Async() => await _inputReader.ParseLinesAsync(7, ParseLine)
+            .OrderBy(x => Strength(x.Cards[4], false))
+            .OrderBy(x => Strength(x.Cards[3], false))
+            .OrderBy(x => Strength(x.Cards[2], false))
+            .OrderBy(x => Strength(x.Cards[1], false))
+            .OrderBy(x => Strength(x.Cards[0], false))
+            .OrderBy(x => Score(x.Cards, false))
             .Select((x, i) => (i + 1) * x.Bid)
             .SumAsync()
             .ToStringAsync();
-    }
 
     [Part2]
-    public async Task<string?> Part2Async()
-    {
-        var rank = 1;
-        await foreach (var hand in _inputReader.ParseLinesAsync(7, ParseLine)
-                           .OrderBy(x => CardStrength2[x.Cards[4]])
-                           .OrderBy(x => CardStrength2[x.Cards[3]])
-                           .OrderBy(x => CardStrength2[x.Cards[2]])
-                           .OrderBy(x => CardStrength2[x.Cards[1]])
-                           .OrderBy(x => CardStrength2[x.Cards[0]])
-                           .OrderBy(x => Score2(x.Cards)))
-        {
-            Console.WriteLine($"{hand}: {rank * hand.Bid}");
-            rank++;
-        }
-
-        return await _inputReader.ParseLinesAsync(7, ParseLine)
-            .OrderBy(x => CardStrength2[x.Cards[4]])
-            .OrderBy(x => CardStrength2[x.Cards[3]])
-            .OrderBy(x => CardStrength2[x.Cards[2]])
-            .OrderBy(x => CardStrength2[x.Cards[1]])
-            .OrderBy(x => CardStrength2[x.Cards[0]])
-            .OrderBy(x => Score2(x.Cards))
+    public async Task<string?> Part2Async() => await _inputReader.ParseLinesAsync(7, ParseLine)
+            .OrderBy(x => Strength(x.Cards[4], true))
+            .OrderBy(x => Strength(x.Cards[3], true))
+            .OrderBy(x => Strength(x.Cards[2], true))
+            .OrderBy(x => Strength(x.Cards[1], true))
+            .OrderBy(x => Strength(x.Cards[0], true))
+            .OrderBy(x => Score(x.Cards, true))
             .Select((x, i) => (i + 1) * x.Bid)
             .SumAsync()
             .ToStringAsync();
-    }
 
-    private Hand ParseLine(string line)
-    {
-        return line.SplitBy(" ", parts => new Hand(
+    private Hand ParseLine(string line) =>
+        line.SplitBy(" ", parts => new Hand(
             parts.First,
             int.Parse(parts.Second)
         ));
+
+    private static int Strength(char c, bool useJokers)
+    {
+        if (useJokers && c == 'J')
+            return 0;
+
+        return c switch
+        {
+            '2' => 1,
+            '3' => 2,
+            '4' => 3,
+            '5' => 4,
+            '6' => 5,
+            '7' => 6,
+            '8' => 7,
+            '9' => 8,
+            'T' => 9,
+            'J' => 10,
+            'Q' => 11,
+            'K' => 12,
+            'A' => 13,
+            _ => throw new NotImplementedException()
+        };
     }
 
-    private static int Score(string cards)
+    private static int Score(string cards, bool useJokers)
     {
         var values = cards.GroupBy(c => c).ToDictionary(kv => kv.Key, kv => kv.Count());
+
+        int jokerCount = 0;
+        if (useJokers)
+            values.TryGetValue('J', out jokerCount);
+
         if (values.Count == 1)
             return 6;
 
         if (values.ContainsValue(4))
-            return 5;
+            return jokerCount > 0 ? 6 : 5;
 
         if (values.ContainsValue(3) && values.ContainsValue(2))
-            return 4;
+            return jokerCount > 0 ? 6 : 4;
 
         if (values.ContainsValue(3))
-            return 3;
-
-        if (values.Values.Count(x => x == 2) == 2)
-            return 2;
-
-        if (values.ContainsValue(2))
-            return 1;
-
-        return 0;
-    }
-
-    private static int Score2(string cards)
-    {
-        var values = cards.GroupBy(c => c).ToDictionary(kv => kv.Key, kv => kv.Count());
-
-        values.TryGetValue('J', out var jokerCount);
-
-        if (values.Count == 1)
-            return 6;
-
-        if (values.ContainsValue(4))
-        {
-            if (jokerCount > 0)
-                return 6;
-
-            return 5;
-        }
-
-        if (values.ContainsValue(3) && values.ContainsValue(2))
-        {
-            if (jokerCount > 0)
-                return 6;
-
-            return 4;
-        }
-
-        if (values.ContainsValue(3))
-        {
-            if (jokerCount > 0)
-                return 5;
-
-            return 3;
-        }
+            return jokerCount > 0 ? 5 : 3;
 
         if (values.Values.Count(x => x == 2) == 2)
             return jokerCount switch
