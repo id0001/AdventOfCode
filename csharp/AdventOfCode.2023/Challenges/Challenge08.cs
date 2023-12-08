c998115dfe939b6be0ce3234815bb0f96643ff59
= <yi.spierings@gmail.com>
2023-12-08 10:20:15 +0100

    Day 8 is done

--- csharp/AdventOfCode.2023/Challenges/Challenge08.cs
+++ csharp/AdventOfCode.2023/Challenges/Challenge08.cs
@@ -1,9 +1,14 @@
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+using System.Xml.Schema;
 using AdventOfCode.Core;
 using AdventOfCode.Core.IO;
+using AdventOfCode.Lib;
+using AdventOfCode.Lib.Math;
 
 namespace AdventOfCode2023.Challenges;
 
-//[Challenge(8)]
+[Challenge(8)]
 public class Challenge08
 {
     private readonly IInputReader _inputReader;
@@ -16,12 +21,68 @@ public class Challenge08
     [Part1]
     public async Task<string> Part1Async()
     {
-        return string.Empty;
+        var input = await ParseInputAsync(8);
+        return CalculateSteps("AAA", input, n => n == "ZZZ").ToString();
     }
 
-    // [Part2]
+    [Part2]
     public async Task<string> Part2Async()
     {
-        return string.Empty;
+        var input = await ParseInputAsync(8);
+        return input.Nodes.Keys
+            .Where(n => n.EndsWith("A"))
+            .Aggregate(1L,
+            (total, next) => Euclid.LeastCommonMultiple(total, CalculateSteps(next, input, n => n.EndsWith("Z")))).ToString();
     }
+
+    private static int CalculateSteps(string currentNode, Input input, Func<string,bool> isFinished)
+    {
+        var index = 0;
+        var steps = 0;
+        while (!isFinished(currentNode))
+        {
+            currentNode = input.Instructions[index] switch
+            {
+                'L' => input.Nodes[currentNode][0],
+                'R' => input.Nodes[currentNode][1],
+                _ => throw new ArgumentOutOfRangeException()
+            };
+
+            steps++;
+            index = (index + 1) % input.Instructions.Length;
+        }
+
+        return steps;
+    }
+
+    private async Task<Input> ParseInputAsync(int day)
+    {
+        var nl = Environment.NewLine;
+        var text = await _inputReader.ReadAllTextAsync(day);
+        return text.SplitBy($"{nl}{nl}", parts =>
+        {
+            return new Input(parts.First,
+                parts.Second
+                    .SplitBy(nl)
+                    .Select(line => Regex.Match(line, @"^(\w{3}) = \((\w{3}), (\w{3})\)$"))
+                    .ToDictionary(kv => kv.Groups[1].Value, kv => new[] {kv.Groups[2].Value, kv.Groups[3].Value}));
+        });
+    }
+    
+    private record Input(string Instructions, Dictionary<string, string[]> Nodes);
+
+    private sealed record Step(string Name, long Index, long InitialSteps)
+    {
+        public bool Equals(Step? other)
+        {
+            if (other is null)
+                return false;
+
+            return Name == other.Name && Index == other.Index;
+        }
+
+        public override int GetHashCode() => HashCode.Combine(Name, Index);
+    }
+
+    private record Cycle(long Offset, long Modulus);
 }
--- csharp/AdventOfCode.Lib/Math/Euclid.Modulus.cs
+++ csharp/AdventOfCode.Lib/Math/Euclid.Modulus.cs
@@ -10,4 +10,19 @@ public static partial class Euclid
 
 	public static BigInteger Modulus(BigInteger dividend, BigInteger divisor) =>
 		(dividend % divisor + divisor) % divisor;
+
+	public static (long, long) ChineseRemainderTheorem(long[] dividends, long[] divisors)
+	{
+		var totalMod = divisors.Product();
+		var total = 0L;
+		for (var i = 0; i < divisors.Length; i++)
+		{
+			var ni = totalMod / divisors[i];
+			var xi = Euclid.ModInverse(ni, divisors[i]);
+			var bi = dividends[i];
+			total += ni * xi * bi;
+		}
+
+		return (total % totalMod, totalMod);
+	}
 }
