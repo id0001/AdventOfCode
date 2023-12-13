37e664f422ad39b0e63c4bbd8a1a7a1ddf922f9b
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2023-12-13 10:38:43 +0100

    Day 13 is done

--- csharp/AdventOfCode.2023/Challenges/Challenge13.cs
+++ csharp/AdventOfCode.2023/Challenges/Challenge13.cs
@@ -1,9 +1,10 @@
 using AdventOfCode.Core;
 using AdventOfCode.Core.IO;
+using AdventOfCode.Lib;
 
 namespace AdventOfCode2023.Challenges;
 
-//[Challenge(13)]
+[Challenge(13)]
 public class Challenge13
 {
     private readonly IInputReader _inputReader;
@@ -16,12 +17,110 @@ public class Challenge13
     [Part1]
     public async Task<string> Part1Async()
     {
-        return string.Empty;
+        var text = await _inputReader.ReadAllTextAsync(13);
+        var nl = Environment.NewLine;
+
+        var cols = 0;
+        var rows = 0;
+        foreach (var block in text.SplitBy($"{nl}{nl}").Select(line => line.SplitBy(nl).ToArray()))
+        {
+            var r = GetReflectedRow(block, false);
+            rows += r;
+            if (r == 0)
+                cols += GetReflectedColumn(block, false);
+        }
+
+        return ((rows * 100) + cols).ToString();
     }
 
-    // [Part2]
+    [Part2]
     public async Task<string> Part2Async()
     {
-        return string.Empty;
+        var text = await _inputReader.ReadAllTextAsync(13);
+        var nl = Environment.NewLine;
+
+        var cols = 0;
+        var rows = 0;
+        foreach (var block in text.SplitBy($"{nl}{nl}").Select(line => line.SplitBy(nl).ToArray()))
+        {
+            var r = GetReflectedRow(block, true);
+            rows += r;
+            if (r == 0)
+                cols += GetReflectedColumn(block, true);
+        }
+
+        return ((rows * 100) + cols).ToString();
+    }
+
+    private int GetReflectedRow(string[] block, bool withSmudge)
+    {
+        for (var y = 0; y < block.Length - 1; y++)
+        {
+            bool noReflection = false;
+            bool foundSmudge = false;
+            for (var x = 0; x < block[y].Length; x++)
+            {
+                for (var s = 0; s <= Math.Min(y, block.Length - 2 - y); s++)
+                {
+                    if (block[y - s][x] != block[y + 1 + s][x])
+                    {
+                        if (withSmudge && !foundSmudge)
+                            foundSmudge = true;
+                        else
+                        {
+                            noReflection = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (noReflection)
+                    break;
+            }
+
+            if (withSmudge && !foundSmudge)
+                continue;
+
+            if (!noReflection)
+                return y + 1;
+        }
+
+        return 0;
+    }
+
+    private int GetReflectedColumn(string[] block, bool withSmudge)
+    {
+        for (var x = 0; x < block[0].Length - 1; x++)
+        {
+            bool noReflection = false;
+            bool foundSmudge = false;
+            for (var y = 0; y < block.Length; y++)
+            {
+                for (var s = 0; s <= Math.Min(x, block[0].Length - 2 - x); s++)
+                {
+                    if (block[y][x - s] != block[y][x + 1 + s])
+                    {
+                        if (withSmudge && !foundSmudge)
+                            foundSmudge = true;
+                        else
+                        {
+                            noReflection = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (noReflection)
+                    break;
+            }
+
+            if (withSmudge && !foundSmudge)
+                continue;
+
+            if (!noReflection)
+                return x + 1;
+        }
+
+        return 0;
     }
 }
