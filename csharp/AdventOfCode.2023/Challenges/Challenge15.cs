079cd8e7a2a753a421d5f576efabae395283f9af
= <yi.spierings@gmail.com>
2023-12-15 11:15:15 +0100

    Day 15 is done

--- csharp/AdventOfCode.2023/Challenges/Challenge15.cs
+++ csharp/AdventOfCode.2023/Challenges/Challenge15.cs
@@ -1,9 +1,10 @@
 using AdventOfCode.Core;
 using AdventOfCode.Core.IO;
+using AdventOfCode.Lib;
 
 namespace AdventOfCode2023.Challenges;
 
-//[Challenge(15)]
+[Challenge(15)]
 public class Challenge15
 {
     private readonly IInputReader _inputReader;
@@ -14,14 +15,51 @@ public class Challenge15
     }
 
     [Part1]
-    public async Task<string> Part1Async()
+    public async Task<string?> Part1Async()
     {
-        return string.Empty;
+        return await _inputReader.ReadLineAsync(15, ',').SumAsync(Hash).ToStringAsync();
     }
 
-    // [Part2]
+    [Part2]
     public async Task<string> Part2Async()
     {
-        return string.Empty;
+        var boxes = Enumerable.Range(0, 256).Select(_ => new List<Lens>()).ToArray();
+        
+        await foreach (var part in _inputReader.ReadLineAsync(15, ','))
+        {
+            if (part.Contains('='))
+            {
+                var lens = part.SplitBy("=", parts => new Lens(parts.First, int.Parse(parts.Second)));
+                var hash = Hash(lens.Label);
+                var index = boxes[hash].IndexOf(lens);
+                if (index < 0)
+                    boxes[hash].Add(lens);
+                else
+                    boxes[hash][index] = lens;
+            }
+            else
+            {
+                var lens = new Lens(part[0..^1], 0);
+                var hash = Hash(lens.Label);
+                var index = boxes[hash].IndexOf(lens);
+                if (index >= 0)
+                    boxes[hash].RemoveAt(index);
+            }
+        }
+
+        return boxes
+            .SelectMany((b, bi) => b
+                .Select((l, li) => (bi + 1) * (li + 1) * l.FocalLength))
+            .Sum()
+            .ToString();
+    }
+
+    private static int Hash(string s) => s.Aggregate(0, (a, b) => (b + a) * 17 % 256);
+
+    private sealed record Lens(string Label, int FocalLength)
+    {
+        public bool Equals(Lens? other) => other is not null && other.Label == Label;
+
+        public override int GetHashCode() => Label.GetHashCode();
     }
 }
