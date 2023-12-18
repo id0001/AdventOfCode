71eaa2e00c5b43705d68537f921a7ccbe46beff0
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2023-12-18 11:10:02 +0100

    Day 18 is done

--- csharp/AdventOfCode.2023/Challenges/Challenge18.cs
+++ csharp/AdventOfCode.2023/Challenges/Challenge18.cs
@@ -2,9 +2,29 @@ using System.Text.RegularExpressions;
 using AdventOfCode.Core;
 using AdventOfCode.Core.IO;
 using AdventOfCode.Lib;
-using AdventOfCode.Lib.Collections;
 
 namespace AdventOfCode2023.Challenges;
+
+/*
+ * Shoelace + Picks theorem
+ * https://en.wikipedia.org/wiki/Pick's_theorem
+ * https://www.101computing.net/the-shoelace-algorithm/
+ * 
+ * Picks (Area of a polygon with only integer points):
+ * i = number of contained integers
+ * b = number of boundary points
+ * Area (A) = i + (b/2) - 1
+ * 
+ * Shoelace gives the area of the polygon, but we want to include the border.
+ * So determine i by plugging the calculated shoelace area into picks algorithm:
+ * i = A - (b/2) = 1
+ * 
+ * And add the border:
+ * i + b
+ * 
+ * Combined:
+ * Ar = As + (b/2) + 1
+ */
 
 [Challenge(18)]
 public class Challenge18
@@ -19,100 +39,35 @@ public class Challenge18
     [Part1]
     public async Task<string> Part1Async()
     {
-        var map = new SparseSpatialMap<Point2, char>
-        {
-            [Point2.Zero] = '.'
-        };
-
-        var current = Point2.Zero;
-        var prevDir = Point2.Zero;
-        var firstDir = Point2.Zero;
-        var count = 0L;
-
+        var vertices = new List<LongPoint>();
+        var current = new LongPoint(0, 0);
+        var circumference = 0L;
         await foreach (var instruction in _inputReader.ParseLinesAsync(18, ParseLine))
         {
-            var pose = new Pose2(current, GetDirection(instruction.Direction));
-
-            if (prevDir != Point2.Zero)
-                map[pose.Position] = GetCorner(prevDir, pose.Face);
-
-            if (firstDir == Point2.Zero)
-                firstDir = pose.Face;
-
-            count += instruction.Amount;
-            for (var i = 0; i < instruction.Amount; i++)
-            {
-                pose = pose.Step();
-                map[pose.Position] = pose.Face.X != 0 ? '-' : '|';
-            }
-
-            current = pose.Position;
-            prevDir = pose.Face;
+            circumference += instruction.Amount;
+            var face = GetDirection(instruction.Direction);
+            current = new LongPoint(current.X + (face.X * instruction.Amount), current.Y + (face.Y * instruction.Amount));
+            vertices.Add(current);
         }
-
-        map[Point2.Zero] = GetCorner(prevDir, firstDir);
-
-        return (count + CountInside(map)).ToString();
+        
+        return (GetArea(vertices) + circumference / 2 + 1).ToString();
     }
 
     [Part2]
     public async Task<string> Part2Async()
     {
-        var map = new Dictionary<LongPoint, char>();
-        var left = 0L;
-        var right = 0L;
-        var top = 0L;
-        var bottom = 0L;
-
-        var current = new LongPoint(0,0);
-        var prevDir = Point2.Zero;
-        var firstDir = Point2.Zero;
-        var count = 0L;
-
-        await foreach (var instruction in _inputReader.ParseLinesAsync(0, ParseLine2))
+        var vertices = new List<LongPoint>();
+        var current = new LongPoint(0, 0);
+        var circumference = 0L;
+        await foreach (var instruction in _inputReader.ParseLinesAsync(18, ParseLine2))
         {
-            var dir = GetDirection(instruction.Direction);
-
-            if (prevDir != Point2.Zero)
-                map[current] = GetCorner(prevDir, dir);
-
-            if (firstDir == Point2.Zero)
-                firstDir = dir;
-
-            count += instruction.Amount;
-            for (var i = 0; i < instruction.Amount; i++)
-            {
-                current = new LongPoint(current.X + dir.X, current.Y + dir.Y);
-                map[current] = dir.X != 0 ? '-' : '|';
-
-                left = Math.Min(left, current.X);
-                top = Math.Min(top, current.Y);
-                right = Math.Max(right, current.X);
-                bottom = Math.Max(bottom, current.Y);
-            }
-
-            prevDir = dir;
+            circumference += instruction.Amount;
+            var face = GetDirection(instruction.Direction);
+            current = new LongPoint(current.X + (face.X * instruction.Amount), current.Y + (face.Y * instruction.Amount));
+            vertices.Add(current);
         }
 
-        map[new LongPoint(0,0)] = GetCorner(prevDir, firstDir);
-
-        return (count + CountInside2(map, new Bounds(left, top, right, bottom))).ToString();
-    }
-
-    private static char GetCorner(Point2 from, Point2 to)
-    {
-        return (from, to) switch
-        {
-            ((1, 0), (0, 1)) => '7',
-            ((1, 0), (0, -1)) => 'J',
-            ((-1, 0), (0, 1)) => 'F',
-            ((-1, 0), (0, -1)) => 'L',
-            ((0, 1), (1, 0)) => 'L',
-            ((0, 1), (-1, 0)) => 'J',
-            ((0, -1), (1, 0)) => 'F',
-            ((0, -1), (-1, 0)) => '7',
-            _ => throw new ArgumentOutOfRangeException()
-        };
+        return (GetArea(vertices) + circumference / 2 + 1).ToString();
     }
 
     private static Point2 GetDirection(char c) => c switch
@@ -124,88 +79,22 @@ public class Challenge18
         _ => throw new ArgumentOutOfRangeException()
     };
 
-    private static long CountInside(SparseSpatialMap<Point2, char> map)
+    private double GetArea(IList<LongPoint> vertices)
     {
-        // Loop over every row and column and count how many vertical borders are crossed.
-        // A point is inside the polygon when the amount of crossed borders is uneven.
-        var inside = 0;
-        var prev = '.';
-        for (var y = map.Bounds.GetMin(1) - 1; y < map.Bounds.GetMax(1) + 1; y++)
+        var vcount = vertices.Count;
+        var sum1 = 0L;
+        var sum2 = 0L;
+
+        for (var i = 0; i < vcount - 1; i++)
         {
-            var bordersCrossed = 0;
-            for (var x = map.Bounds.GetMin(0) - 1; x < map.Bounds.GetMax(0) + 1; x++)
-            {
-                var p = new Point2(x, y);
-                if (map.ContainsKey(p))
-                {
-                    if (map[p] == '7' && prev == 'L') // Special case L7 does not cross borders twice
-                    {
-                        prev = map[p];
-                        continue;
-                    }
-
-                    if (map[p] == 'J' && prev == 'F') // Special case FJ does not cross borders twice
-                    {
-                        prev = map[p];
-                        continue;
-                    }
-
-                    if (map[p] == '-') // Special case - does not cross any border
-                        continue;
-
-                    prev = map[p];
-                    bordersCrossed++;
-                    continue;
-                }
-
-                if (bordersCrossed % 2 == 1)
-                    inside++;
-            }
+            sum1 += vertices[i].X * vertices[i + 1].Y;
+            sum2 += vertices[i].Y * vertices[i + 1].X;
         }
 
-        return inside;
-    }
-    
-    private static long CountInside2(Dictionary<LongPoint, char> map, Bounds bounds)
-    {
-        // Loop over every row and column and count how many vertical borders are crossed.
-        // A point is inside the polygon when the amount of crossed borders is uneven.
-        var inside = 0;
-        var prev = '.';
-        for (var y = bounds.Top; y <= bounds.Bottom; y++)
-        {
-            var bordersCrossed = 0;
-            for (var x = bounds.Left; x <= bounds.Right; x++)
-            {
-                var p = new LongPoint(x, y);
-                if (map.ContainsKey(p))
-                {
-                    if (map[p] == '7' && prev == 'L') // Special case L7 does not cross borders twice
-                    {
-                        prev = map[p];
-                        continue;
-                    }
+        sum1 += vertices[vcount - 1].X * vertices[0].Y;
+        sum2 += vertices[vcount - 1].Y * vertices[0].X;
 
-                    if (map[p] == 'J' && prev == 'F') // Special case FJ does not cross borders twice
-                    {
-                        prev = map[p];
-                        continue;
-                    }
-
-                    if (map[p] == '-') // Special case - does not cross any border
-                        continue;
-
-                    prev = map[p];
-                    bordersCrossed++;
-                    continue;
-                }
-
-                if (bordersCrossed % 2 == 1)
-                    inside++;
-            }
-        }
-
-        return inside;
+        return Math.Abs(sum1 - sum2) / 2d;
     }
 
     private static Instruction ParseLine(string line)
@@ -219,7 +108,7 @@ public class Challenge18
 
     private static Instruction ParseLine2(string line)
     {
-        var dirs = new[] {'R', 'D', 'L', 'U'};
+        var dirs = new[] { 'R', 'D', 'L', 'U' };
 
         return line.SplitBy(" ")
             .Transform(parts =>
@@ -236,6 +125,4 @@ public class Challenge18
     private record Instruction(char Direction, long Amount);
 
     private record LongPoint(long X, long Y);
-
-    private record Bounds(long Left, long Top, long Right, long Bottom);
 }
