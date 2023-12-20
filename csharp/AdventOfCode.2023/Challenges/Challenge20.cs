866772442e035a560611e4c91c8b765bfe03c413
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2023-12-20 09:46:44 +0100

    Day 20 is done

--- csharp/AdventOfCode.2023/Challenges/Challenge20.cs
+++ csharp/AdventOfCode.2023/Challenges/Challenge20.cs
@@ -1,9 +1,10 @@
 using AdventOfCode.Core;
 using AdventOfCode.Core.IO;
+using AdventOfCode.Lib;
 
 namespace AdventOfCode2023.Challenges;
 
-//[Challenge(20)]
+[Challenge(20)]
 public class Challenge20
 {
     private readonly IInputReader _inputReader;
@@ -16,12 +17,106 @@ public class Challenge20
     [Part1]
     public async Task<string> Part1Async()
     {
-        return string.Empty;
+        var modules = await _inputReader.ParseLinesAsync(20, ParseLine).ToDictionaryAsync(kv => kv.Name);
+        var flipflops = modules.Values.Where(m => m.Type == "%").ToDictionary(kv => kv.Name, kv => false);
+        var conjunctions = modules.Values.Where(m => m.Type == "&").ToDictionary(kv => kv.Name, kv => modules.Values.Where(m => m.Destinations.Contains(kv.Name)).ToDictionary(kv => kv.Name, kv => false));
+
+        var rxPresses = conjunctions["bn"].ToDictionary(kv => kv.Key, kv => 0L);
+
+        var lows = 0L;
+        var highs = 0L;
+        var presses = 0L;
+        while(presses < 1000L)
+            Process(modules, flipflops, conjunctions, rxPresses, ref presses, ref lows, ref highs);
+
+        return (lows * highs).ToString();
     }
 
-    // [Part2]
+    [Part2]
     public async Task<string> Part2Async()
     {
-        return string.Empty;
+        var modules = await _inputReader.ParseLinesAsync(20, ParseLine).ToDictionaryAsync(kv => kv.Name);
+        var flipflops = modules.Values.Where(m => m.Type == "%").ToDictionary(kv => kv.Name, kv => false);
+        var conjunctions = modules.Values.Where(m => m.Type == "&").ToDictionary(kv => kv.Name, kv => modules.Values.Where(m => m.Destinations.Contains(kv.Name)).ToDictionary(kv => kv.Name, kv => false));
+
+        var rxPresses = conjunctions["bn"].ToDictionary(kv => kv.Key, kv => 0L);
+
+        var i = 0L;
+        var lows = 0L;
+        var highs = 0L;
+        while (true)
+        {
+            Process(modules, flipflops, conjunctions, rxPresses, ref i, ref lows, ref highs);
+            if (rxPresses.Values.All(s => s > 0))
+                return rxPresses.Values.Product().ToString();
+        }
+
+        throw new InvalidOperationException();
     }
+
+    private void Process(Dictionary<string, Module> modules, Dictionary<string, bool> flipflops, Dictionary<string, Dictionary<string, bool>> conjunctions, Dictionary<string, long> rxPresses, ref long presses, ref long lows, ref long highs)
+    {
+        presses++;
+
+        var queue = new Queue<Signal>();
+
+        lows++;
+        foreach (var d in modules["broadcaster"].Destinations)
+            queue.Enqueue(new Signal("broadcaster", d, false));
+
+        while (queue.Count > 0)
+        {
+            var (source, dest, pulseIn) = queue.Dequeue();
+            //Console.WriteLine($"{source} -{(pulseIn ? "high" : "low")}-> {dest}");
+            if (pulseIn)
+                highs++;
+            else
+                lows++;
+
+            if (!modules.ContainsKey(dest))
+                continue;
+
+            var (type, _, next) = modules[dest];
+            var pulseOut = (type, pulseIn) switch
+            {
+                ("broadcaster", _) => pulseIn,
+                ("%", false) => flipflops[dest] = !flipflops[dest],
+                ("&", _) => Conjuction(conjunctions, source, dest, pulseIn),
+                _ => (bool?)null
+            };
+
+            if (!pulseOut.HasValue)
+                continue;
+
+            if (type == "&" && modules[dest].Destinations.Contains("rx"))
+                foreach (var kv in conjunctions[dest])
+                    if (kv.Value)
+                        rxPresses[kv.Key] = presses;
+
+            foreach (var d in modules[dest].Destinations)
+                queue.Enqueue(new Signal(dest, d, pulseOut.Value));
+        }
+    }
+
+    private static bool Conjuction(Dictionary<string, Dictionary<string, bool>> conjunctions, string source, string dest, bool pulseIn)
+    {
+        conjunctions[dest][source] = pulseIn;
+        return !conjunctions[dest].Values.All(s => s);
+    }
+
+    private static Module ParseLine(string line)
+    {
+        return line
+            .SplitBy("->")
+            .Transform(parts =>
+            {
+                int strip = parts.First()[0] is '%' or '&' ? 1 : 0;
+                string type = strip > 0 ? parts.First()[0].ToString() : parts.First();
+                return new Module(type, parts.First()[strip..], parts.Second().SplitBy(","));
+            });
+    }
+
+    private record Module(string Type, string Name, string[] Destinations);
+
+    private record Signal(string Source, string Destination, bool PulseIn);
 }
