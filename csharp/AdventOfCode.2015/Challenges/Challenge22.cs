f39c55311c654d8942eb57729bbf81052dd66540
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2024-01-24 16:04:52 +0100

    2015 day 22 is done | Astar changes

---
+++ csharp/AdventOfCode.2015/Challenges/Challenge22.cs
@@ -0,0 +1,94 @@
+using AdventOfCode.Core;
+using AdventOfCode.Lib.PathFinding;
+
+namespace AdventOfCode2015.Challenges;
+
+[Challenge(22)]
+public class Challenge22
+{
+    private const int EnemyDamage = 9;
+
+    [Part1]
+    public string Part1()
+    {
+        var astar = new AStar<State>(n => GetAdjacent(n, false), Weight);
+
+        astar.TryPath(new State(50, 58, 500, 0, 0, 0, 0), IsFinished, out _, out var minSpent);
+        return minSpent.ToString();
+    }
+
+    [Part2]
+    public string Part2()
+    {
+        var astar = new AStar<State>(n => GetAdjacent(n, true), Weight);
+
+        astar.TryPath(new State(50, 58, 500, 0, 0, 0, 0), IsFinished, out _, out var minSpent);
+        return minSpent.ToString();
+    }
+
+    private int Weight(State current, State next) => next.Spent - current.Spent;
+
+    private bool IsFinished(State state)
+    {
+        return state.EnemyHp <= 0;
+    }
+
+    private IEnumerable<State> GetAdjacent(State current, bool hardMode)
+    {
+        if (hardMode)
+            current = current with { PlayerHp = current.PlayerHp - 1 };
+
+        if (current.PlayerHp <= 0) // No paths
+            yield break;
+
+        var (playerHp, enemyHp, mana, spent, shield, poison, recharge) = ApplyEffects(current);
+
+        // Magic missile
+        if (mana >= 53)
+            yield return EnemyTurn(new State(playerHp, enemyHp - 4, mana - 53, spent + 53, shield, poison, recharge));
+
+        // Drain
+        if (mana >= 73)
+            yield return EnemyTurn(new State(playerHp + 2, enemyHp - 2, mana - 73, spent + 73, shield, poison, recharge));
+
+        // Shield
+        if (mana >= 113 && shield == 0)
+            yield return EnemyTurn(new State(playerHp, enemyHp, mana - 113, spent + 113, 6, poison, recharge));
+
+        // Poison
+        if (mana >= 173 && poison == 0)
+            yield return EnemyTurn(new State(playerHp, enemyHp, mana - 173, spent + 173, shield, 6, recharge));
+
+        // Recharge
+        if (mana >= 229 && recharge == 0)
+            yield return EnemyTurn(new State(playerHp, enemyHp, mana - 229, spent + 229, shield, poison, 5));
+    }
+
+    private State ApplyEffects(State current)
+    {
+        if (current.Poison > 0)
+            current = current with { EnemyHp = current.EnemyHp - 3, Poison = current.Poison - 1 };
+
+        if (current.Recharge > 0)
+            current = current with { Mana = current.Mana + 101, Recharge = current.Recharge - 1 };
+
+        if (current.Shield > 0)
+            current = current with { Shield = current.Shield - 1 };
+
+        return current;
+    }
+
+    private State EnemyTurn(State current)
+    {
+        // Get armor
+        int armor = current.Shield > 0 ? 7 : 0;
+
+        // Apply effects
+        current = ApplyEffects(current);
+
+        // Do damage
+        return current with { PlayerHp = current.PlayerHp - Math.Max(1, EnemyDamage - armor) };
+    }
+
+    private record State(int PlayerHp, int EnemyHp, int Mana, int Spent, int Shield, int Poison, int Recharge);
+}
--- csharp/AdventOfCode.Lib/PathFinding/AStar.cs
+++ csharp/AdventOfCode.Lib/PathFinding/AStar.cs
@@ -1,9 +1,9 @@
 ﻿namespace AdventOfCode.Lib.PathFinding;
 
-public class AStar<T>(Func<T, IEnumerable<T>> adjecent, Func<T, T, int> weight, Func<T, int> heuristic)
+public class AStar<T>(Func<T, IEnumerable<T>> adjacent, Func<T, T, int> weight, Func<T, int> heuristic)
     where T : notnull
 {
-    public AStar(Func<T, IEnumerable<T>> adjecent, Func<T, T, int> weight) : this(adjecent, weight, _ => 0)
+    public AStar(Func<T, IEnumerable<T>> adjacent, Func<T, T, int> weight) : this(adjacent, weight, _ => 0)
     {
     }
 
@@ -13,21 +13,10 @@ public class AStar<T>(Func<T, IEnumerable<T>> adjecent, Func<T, T, int> weight, 
         out IEnumerable<T> path,
         out int totalCost
     )
-        => TryPath(start, adjecent, weight, heuristic, isFinished, out path, out totalCost);
-
-    private static bool TryPath(
-        T start,
-        Func<T, IEnumerable<T>> getAdjacent,
-        Func<T, T, int> weight,
-        Func<T, int> heuristic,
-        Func<T, bool> isFinished,
-        out IEnumerable<T> path,
-        out int totalCost
-    )
     {
         var queue = new PriorityQueue<T, int>();
         var cameFrom = new Dictionary<T, T>();
-        var costSoFar = new Dictionary<T, int> {{start, 0}};
+        var costSoFar = new Dictionary<T, int> { { start, 0 } };
 
         queue.Enqueue(start, 0);
 
@@ -42,21 +31,53 @@ public class AStar<T>(Func<T, IEnumerable<T>> adjecent, Func<T, T, int> weight, 
                 return true;
             }
 
-            foreach (var adjacent in getAdjacent(currentNode))
+            foreach (var nextNode in adjacent(currentNode))
             {
-                var newCost = costSoFar[currentNode] + weight(currentNode, adjacent);
+                var newCost = costSoFar[currentNode] + weight(currentNode, nextNode);
 
-                if (!costSoFar.ContainsKey(adjacent) || newCost < costSoFar[adjacent])
+                if (!costSoFar.ContainsKey(nextNode) || newCost < costSoFar[nextNode])
                 {
-                    costSoFar[adjacent] = newCost;
-                    queue.Enqueue(adjacent, newCost + heuristic(adjacent));
-                    cameFrom[adjacent] = currentNode;
+                    costSoFar[nextNode] = newCost;
+                    queue.Enqueue(nextNode, newCost + heuristic(nextNode));
+                    cameFrom[nextNode] = currentNode;
                 }
             }
         }
 
         path = Enumerable.Empty<T>();
         totalCost = 0;
+        return false;
+    }
+
+    public bool All(
+         T start,
+        Func<T, bool> shouldContinue
+    )
+    {
+        var queue = new PriorityQueue<T, int>();
+        var costSoFar = new Dictionary<T, int> { { start, 0 } };
+
+        queue.Enqueue(start, 0);
+
+        while (queue.Count > 0)
+        {
+            var currentNode = queue.Dequeue();
+
+            if (!shouldContinue(currentNode))
+                continue;
+
+            foreach (var nextNode in adjacent(currentNode))
+            {
+                var newCost = costSoFar[currentNode] + weight(currentNode, nextNode);
+
+                if (!costSoFar.ContainsKey(nextNode) || newCost < costSoFar[nextNode])
+                {
+                    costSoFar[nextNode] = newCost;
+                    queue.Enqueue(nextNode, newCost + heuristic(nextNode));
+                }
+            }
+        }
+
         return false;
     }
 
