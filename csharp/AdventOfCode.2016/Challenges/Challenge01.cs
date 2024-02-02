2d09f0d479787be6816b10100fe3398ae1a5aaa6
= <yi.spierings@gmail.com>
2024-02-02 14:32:28 +0100

    2016 day 1 and 2

--- csharp/AdventOfCode.2016/Challenges/Challenge01.cs
+++ csharp/AdventOfCode.2016/Challenges/Challenge01.cs
@@ -1,20 +1,43 @@
 using AdventOfCode.Core;
 using AdventOfCode.Core.IO;
+using AdventOfCode.Lib;
 
 namespace AdventOfCode2016.Challenges;
 
-//[Challenge(1)]
+[Challenge(1)]
 public class Challenge01(IInputReader inputReader)
 {
     [Part1]
-    public async Task<string> Part1Async()
-    {
-        return string.Empty;
-    }
+    public async Task<string> Part1Async() => Point2.ManhattanDistance(Point2.Zero,
+        (await inputReader.ReadAllTextAsync(1))
+        .SplitBy(", ")
+        .Aggregate(new Pose2(Point2.Zero, Face.Up),
+            (pose, line) => (line[0], line[1..].As<int>()) switch
+            {
+                ('R', var a) => pose.TurnRight().Step(a),
+                ('L', var a) => pose.TurnLeft().Step(a),
+                _ => throw new NotImplementedException()
+            })
+        .Position).ToString();
 
-    // [Part2]
+    [Part2]
     public async Task<string> Part2Async()
     {
+        HashSet<Point2> visited = [Point2.Zero];
+        var pose = new Pose2(Point2.Zero, Face.Up);
+
+        var text = await inputReader.ReadAllTextAsync(1);
+        foreach (var (dir, amount) in text.SplitBy(", ").Select(line => (line[0], line[1..].As<int>())))
+        {
+            pose = dir == 'R' ? pose.TurnRight() : pose.TurnLeft();
+            for (var i = 0; i < amount; i++)
+            {
+                pose = pose.Step();
+                if (!visited.Add(pose.Position))
+                    return Point2.ManhattanDistance(Point2.Zero, pose.Position).ToString();
+            }
+        }
+
         return string.Empty;
     }
 }
--- csharp/AdventOfCode.2016/Challenges/Challenge02.cs
+++ csharp/AdventOfCode.2016/Challenges/Challenge02.cs
@@ -1,20 +1,52 @@
 using AdventOfCode.Core;
 using AdventOfCode.Core.IO;
+using AdventOfCode.Lib;
 
 namespace AdventOfCode2016.Challenges;
 
-//[Challenge(2)]
+[Challenge(2)]
 public class Challenge02(IInputReader inputReader)
 {
     [Part1]
     public async Task<string> Part1Async()
     {
-        return string.Empty;
+        var keypad = new[,]
+        {
+            {'1', '2', '3'},
+            {'3', '4', '5'},
+            {'7', '8', '9'}
+        };
+
+        return string.Join(string.Empty, await inputReader.ReadLinesAsync(2)
+            .Select(x => GetNumber(keypad, keypad.Bounds(), x))
+            .ToArrayAsync());
     }
 
-    // [Part2]
+    [Part2]
     public async Task<string> Part2Async()
     {
-        return string.Empty;
+        var keypad = new[,]
+        {
+            {'.', '.', '1', '.', '.'},
+            {'.', '2', '3', '4', '.'},
+            {'5', '6', '7', '8', '9'},
+            {'.', 'A', 'B', 'C', '.'},
+            {'.', '.', 'D', '.', '.'}
+        };
+
+        return string.Join(string.Empty, await inputReader.ReadLinesAsync(2)
+            .Select(x => GetNumber(keypad, keypad.Bounds(), x))
+            .ToArrayAsync());
     }
+
+    private static char GetNumber(char[,] keypad, Rectangle bounds, IEnumerable<char> code) => code.Aggregate(
+            new Point2(1, 1), (a, b) => b switch
+            {
+                'U' when bounds.Contains(a.Up) && keypad[a.Up.Y, a.Up.X] != '.' => a.Up,
+                'R' when bounds.Contains(a.Right) && keypad[a.Right.Y, a.Right.X] != '.' => a.Right,
+                'D' when bounds.Contains(a.Down) && keypad[a.Down.Y, a.Down.X] != '.' => a.Down,
+                'L' when bounds.Contains(a.Left) && keypad[a.Left.Y, a.Left.X] != '.' => a.Left,
+                _ => a
+            })
+        .Into(p => keypad[p.Y, p.X]);
 }
