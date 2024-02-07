d98357af489b67a000b4438da639d0af782959a2
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2024-02-07 15:52:25 +0100

    2016 day 3

--- csharp/AdventOfCode.2016/Challenges/Challenge03.cs
+++ csharp/AdventOfCode.2016/Challenges/Challenge03.cs
@@ -1,20 +1,32 @@
 using AdventOfCode.Core;
 using AdventOfCode.Core.IO;
+using AdventOfCode.Lib;
 
 namespace AdventOfCode2016.Challenges;
 
-//[Challenge(3)]
+[Challenge(3)]
 public class Challenge03(IInputReader inputReader)
 {
     [Part1]
-    public async Task<string> Part1Async()
-    {
-        return string.Empty;
-    }
+    public async Task<string> Part1Async() => await inputReader
+            .ReadLinesAsync(3)
+            .Select(line => line.SplitBy(" ").As<int>())
+            .CountAsync(arr =>
+                arr.First() + arr.Second() > arr.Third()
+                && arr.Second() + arr.Third() > arr.First()
+                && arr.Third() + arr.First() > arr.Second())
+            .ToStringAsync();
 
-    // [Part2]
-    public async Task<string> Part2Async()
-    {
-        return string.Empty;
-    }
+    [Part2]
+    public async Task<string> Part2Async() => (await inputReader
+            .ReadLinesAsync(3)
+            .Select(line => line.SplitBy(" ").As<int>())
+            .ToListAsync())
+            .Into(lines => lines.Select(l => l[0]).Concat(lines.Select(l => l[1]).Concat(lines.Select(l => l[2]))))
+            .Chunk(3)
+            .Count(arr =>
+                arr.First() + arr.Second() > arr.Third()
+                && arr.Second() + arr.Third() > arr.First()
+                && arr.Third() + arr.First() > arr.Second()
+            ).ToString();
 }
--- csharp/AdventOfCode.Lib/Extensions/TaskExtensions.cs
+++ csharp/AdventOfCode.Lib/Extensions/TaskExtensions.cs
@@ -2,13 +2,13 @@
 
 public static class TaskExtensions
 {
-    public static async Task<string?> ToStringAsync<T>(this Task<T> task)
+    public static async Task<string> ToStringAsync<T>(this Task<T> task)
     {
-        return (await task)?.ToString();
+        return (await task)?.ToString() ?? string.Empty;
     }
 
-    public static async ValueTask<string?> ToStringAsync<T>(this ValueTask<T> task)
+    public static async ValueTask<string> ToStringAsync<T>(this ValueTask<T> task)
     {
-        return (await task)?.ToString();
+        return (await task)?.ToString() ?? string.Empty;
     }
 }
