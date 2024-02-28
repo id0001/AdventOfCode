26c9c5d371e821d87c0d041e309ca0770556ed11
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2024-02-28 15:09:19 +0100

    2016 day 5

--- csharp/AdventOfCode.2016/Challenges/Challenge05.cs
+++ csharp/AdventOfCode.2016/Challenges/Challenge05.cs
@@ -1,20 +1,56 @@
 using AdventOfCode.Core;
-using AdventOfCode.Core.IO;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace AdventOfCode2016.Challenges;
 
-//[Challenge(5)]
-public class Challenge05(IInputReader inputReader)
+[Challenge(5)]
+public class Challenge05()
 {
+    private const string Input = "abbhdwsy";
+
     [Part1]
-    public async Task<string> Part1Async()
+    public string Part1()
     {
-        return string.Empty;
+        using var md5 = MD5.Create();
+
+        StringBuilder pass = new();
+        var index = 0;
+        while(pass.Length < 8)
+        {
+            var hash = md5.ComputeHash(Encoding.Default.GetBytes(Input + index.ToString()));
+            if (hash[0] == 0 && hash[1] == 0 && (hash[2] & 0xF0) == 0)
+                pass.Append((hash[2] & 0x0F).ToString("X"));
+
+            index++;
+        }
+
+        return pass.ToString().ToLowerInvariant();
     }
 
-    // [Part2]
-    public async Task<string> Part2Async()
+    [Part2]
+    public string Part2()
     {
-        return string.Empty;
+        using var md5 = MD5.Create();
+
+        var pass = new char[8];
+        var charsFound = 0;
+        var index = 0;
+        while (charsFound < 8)
+        {
+            var hash = md5.ComputeHash(Encoding.Default.GetBytes(Input + index.ToString()));
+            if (hash[0] == 0 && hash[1] == 0 && (hash[2] & 0xF0) == 0)
+            {
+                if(int.TryParse((hash[2] & 0x0F).ToString("X"), out var i) && i <= 7 && pass[i] == default)
+                {
+                    pass[i] = (hash[3] & 0xF0).ToString("X")[0];
+                    charsFound++;
+                }
+            }
+
+            index++;
+        }
+
+        return string.Join(string.Empty, pass).ToLowerInvariant();
     }
 }
