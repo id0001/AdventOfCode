896f14b90eede2425c1694229486bc9dd1a31dc1
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2024-03-12 13:08:56 +0100

    2016 day 7

--- csharp/AdventOfCode.2016/Challenges/Challenge07.cs
+++ csharp/AdventOfCode.2016/Challenges/Challenge07.cs
@@ -1,20 +1,105 @@
 using AdventOfCode.Core;
 using AdventOfCode.Core.IO;
+using AdventOfCode.Lib;
 
 namespace AdventOfCode2016.Challenges;
 
-//[Challenge(7)]
+[Challenge(7)]
 public class Challenge07(IInputReader inputReader)
 {
     [Part1]
     public async Task<string> Part1Async()
     {
-        return string.Empty;
+        return await inputReader
+            .ReadLinesAsync(7)
+            .Where(SupportsTls)
+            .CountAsync()
+            .ToStringAsync();
     }
 
-    // [Part2]
+    [Part2]
     public async Task<string> Part2Async()
     {
-        return string.Empty;
+        return await inputReader
+            .ReadLinesAsync(7)
+            .Where(SupportsSsl)
+            .CountAsync()
+            .ToStringAsync();
+    }
+
+    private bool SupportsTls(string line)
+    {
+        var hasAbba = false;
+        var inHypernetSequence = false;
+        var iSinceStart = 0;
+
+        for (var i = 0; i < line.Length; i++)
+        {
+            switch (line[i])
+            {
+                case '[':
+                    inHypernetSequence = true;
+                    iSinceStart = 0;
+                    break;
+                case ']':
+                    inHypernetSequence = false;
+                    iSinceStart = 0;
+                    break;
+                default:
+                    if (iSinceStart++ < 3)
+                        continue;
+
+                    if (line[i - 3] == line[i] && line[i - 2] == line[i - 1] && line[i] != line[i - 1])
+                    {
+                        if (inHypernetSequence)
+                            return false;
+                        hasAbba = true;
+                    }
+                    break;
+            }
+        }
+
+        return hasAbba;
+    }
+
+    private bool SupportsSsl(string line)
+    {
+        var abaSequences = new HashSet<string>();
+        var babSequences = new HashSet<string>();
+        var inHypernetSequence = false;
+        var iSinceStart = 0;
+
+        for (var i = 0; i < line.Length; i++)
+        {
+            switch (line[i])
+            {
+                case '[':
+                    inHypernetSequence = true;
+                    iSinceStart = 0;
+                    break;
+                case ']':
+                    inHypernetSequence = false;
+                    iSinceStart = 0;
+                    break;
+                default:
+                    if (iSinceStart++ < 2)
+                        continue;
+
+                    if (line[i] == line[i - 2] && line[i] != line[i-1])
+                    {
+                        if (inHypernetSequence)
+                            babSequences.Add(new string([line[i - 1], line[i], line[i - 1]]));
+                        else
+                            abaSequences.Add(line[(i - 2)..(i+1)]);
+
+                        if (abaSequences.Intersect(babSequences).Count() > 0)
+                            return true;
+                    }
+
+                    break;
+            }
+        }
+
+        return false;
     }
 }
