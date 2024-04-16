6f9b35fb8281cb9b7043bc6cabd86704e391442a
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2024-04-16 09:03:00 +0200

    2016 day 16

--- csharp/AdventOfCode.2016/Challenges/Challenge16.cs
+++ csharp/AdventOfCode.2016/Challenges/Challenge16.cs
@@ -1,20 +1,61 @@
 using AdventOfCode.Core;
-using AdventOfCode.Core.IO;
+using AdventOfCode.Lib;
+using System.Text;
 
 namespace AdventOfCode2016.Challenges;
 
-//[Challenge(16)]
-public class Challenge16(IInputReader inputReader)
+[Challenge(16)]
+public class Challenge16()
 {
+    private const string Input = "11100010111110100";
+
     [Part1]
     public async Task<string> Part1Async()
     {
-        return string.Empty;
+        string input = Input;
+        int length = 272;
+        while (input.Length < length)
+            input = CreateDragonCurve(input);
+
+        return CreateChecksum(input[0..length]);
     }
 
-    // [Part2]
+    [Part2]
     public async Task<string> Part2Async()
     {
-        return string.Empty;
+        string input = Input;
+        int length = 35651584;
+        while (input.Length < length)
+            input = CreateDragonCurve(input);
+
+        return CreateChecksum(input[0..length]);
+    }
+
+    private string CreateDragonCurve(string input)
+    {
+        var sb = new StringBuilder();
+        for (var i = input.Length - 1; i >= 0; i--)
+            sb.Append(input[i] == '0' ? '1' : '0');
+
+        return input + "0" + sb.ToString();
+    }
+
+    private string CreateChecksum(string input)
+    {
+        var sb = new StringBuilder();
+
+        do
+        {
+            foreach (var pair in input.Chunk(2))
+            {
+                sb.Append(pair[0] == pair[1] ? '1' : '0');
+            }
+
+            input = sb.ToString();
+            sb.Clear();
+        }
+        while (input.Length % 2 == 0);
+
+        return input;
     }
 }
