fa5dc5b67de7c72f0253263a3efa70b7cc1d829e
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2024-05-01 13:04:03 +0200

    2016 day 20

--- csharp/AdventOfCode.2016/Challenges/Challenge20.cs
+++ csharp/AdventOfCode.2016/Challenges/Challenge20.cs
@@ -1,20 +1,47 @@
 using AdventOfCode.Core;
 using AdventOfCode.Core.IO;
+using AdventOfCode.Lib;
 
 namespace AdventOfCode2016.Challenges;
 
-//[Challenge(20)]
+[Challenge(20)]
 public class Challenge20(IInputReader inputReader)
 {
     [Part1]
     public async Task<string> Part1Async()
     {
-        return string.Empty;
+        var ranges = await inputReader.ParseLinesAsync(20, ParseLine).OrderBy(x => x.Start).ToListAsync();
+
+        var i = 0L;
+        for(var ri = 0; ri < ranges.Count; ri++)
+        {
+            if (i < ranges[ri].Start)
+                return i.ToString();
+
+            i = Math.Max(i, ranges[ri].End + 1);
+        }
+
+        return "no answer";
     }
 
-    // [Part2]
+    [Part2]
     public async Task<string> Part2Async()
     {
-        return string.Empty;
+        var ranges = await inputReader.ParseLinesAsync(20, ParseLine).OrderBy(x => x.Start).ToListAsync();
+
+        var (i, count) = (0L, 0L);
+        for (var ri = 0; ri < ranges.Count; ri++)
+        {
+            if (i < ranges[ri].Start)
+                count += (ranges[ri].Start-i);
+
+            i = Math.Max(i, ranges[ri].End + 1);
+        }
+
+        return count.ToString();
     }
+
+    private LongRange ParseLine(string line) => line.SplitBy("-").As<long>().Into(x => new LongRange(x.First(), x.Second()));
+
+    private record LongRange(long Start, long End);
 }
