cb1365e4618ccaaef01ea48223ec57538cb265d6
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2024-05-07 15:32:25 +0200

    2016 day 25

--- csharp/AdventOfCode.2016/Challenges/Challenge25.cs
+++ csharp/AdventOfCode.2016/Challenges/Challenge25.cs
@@ -3,18 +3,34 @@ using AdventOfCode.Core.IO;
 
 namespace AdventOfCode2016.Challenges;
 
-//[Challenge(25)]
+[Challenge(25)]
 public class Challenge25(IInputReader inputReader)
 {
     [Part1]
     public async Task<string> Part1Async()
     {
-        return string.Empty;
-    }
+        var num = 182 * 14;
 
-    // [Part2]
-    public async Task<string> Part2Async()
-    {
+        for(var i = 0; i < num; i++)
+        {
+            var bin = Convert.ToString(num + i, 2);
+            var one = true;
+            var err = false;
+            for(var j = 0; j < bin.Length; j++)
+            {
+                if((one && bin[j] != '1') || (!one && bin[j] == '1'))
+                {
+                    err = true;
+                    break;
+                }
+
+                one = !one;
+            }
+
+            if (!err)
+                return i.ToString();
+        }
+
         return string.Empty;
     }
 }
