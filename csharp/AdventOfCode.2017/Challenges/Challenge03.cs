a3d27cc8182fc0883e12ae9de955a336cbeb469c
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2024-05-14 10:16:34 +0200

    2017 day 3

--- csharp/AdventOfCode.2017/Challenges/Challenge03.cs
+++ csharp/AdventOfCode.2017/Challenges/Challenge03.cs
@@ -1,20 +1,81 @@
 using AdventOfCode.Core;
-using AdventOfCode.Core.IO;
+using AdventOfCode.Lib;
+using AdventOfCode.Lib.Collections;
 
 namespace AdventOfCode2017.Challenges;
 
-//[Challenge(3)]
-public class Challenge03(IInputReader inputReader)
+[Challenge(3)]
+public class Challenge03()
 {
+    private const int Input = 265149;
+    private static readonly Point2[] Directions = [Face.Right, Face.Up, Face.Left, Face.Down];
+
+
     [Part1]
-    public async Task<string> Part1Async()
+    public string Part1()
     {
-        return string.Empty;
+        var curr = Point2.Zero;
+        var di = 0;
+        var totalSteps = 1;
+        var stepChange = 2;
+        var tsi = 0;
+        var sci = 0;
+
+        for(var i = 1; i < Input; i++)
+        {
+            curr += Directions[di];
+            tsi++;
+
+            if(tsi == totalSteps)
+            {
+                di = (di + 1).Mod(4);
+                tsi = 0;
+                sci++;
+            }
+
+            if (sci == stepChange)
+            {
+                totalSteps++;
+                sci = 0;
+            }
+        }
+
+        return Point2.ManhattanDistance(Point2.Zero, curr).ToString();
     }
 
-    // [Part2]
-    public async Task<string> Part2Async()
+    [Part2]
+    public string Part2()
     {
-        return string.Empty;
+        var cloud = new SparseSpatialMap<Point2, int, int>();
+        var curr = Point2.Zero;
+        cloud[curr] = 1;
+        var di = 0;
+        var totalSteps = 1;
+        var stepChange = 2;
+        var tsi = 0;
+        var sci = 0;
+
+        while(true)
+        {
+            curr += Directions[di];
+            cloud[curr] = curr.GetNeighbors(true).Select(n => cloud[n]).Sum();
+            if (cloud[curr] > Input)
+                return cloud[curr].ToString();
+
+            tsi++;
+
+            if (tsi == totalSteps)
+            {
+                di = (di + 1).Mod(4);
+                tsi = 0;
+                sci++;
+            }
+
+            if (sci == stepChange)
+            {
+                totalSteps++;
+                sci = 0;
+            }
+        }
     }
 }
