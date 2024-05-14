2d2b0756c0f983ead41f24f2d795f3b55699884f
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2024-05-14 09:44:33 +0200

    Moved async extensions to linq equivalent classes

--- csharp/AdventOfCode.Lib/Extensions/Linq/EnumerableExtensions.ChunkBy.cs
+++ csharp/AdventOfCode.Lib/Extensions/Linq/EnumerableExtensions.ChunkBy.cs
@@ -16,5 +16,20 @@
                     list = new List<T>();
                 }
         }
+
+        public static async IAsyncEnumerable<IEnumerable<T>> ChunkBy<T>(this IAsyncEnumerable<T> source, T separator)
+        {
+            var list = new List<T>();
+            await foreach (var item in source)
+                if (!EqualityComparer<T>.Default.Equals(item, separator))
+                {
+                    list.Add(item);
+                }
+                else
+                {
+                    yield return list;
+                    list = new List<T>();
+                }
+        }
     }
 }
