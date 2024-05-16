ccd0eb9b120fca7f660640431066d6d0b2789d52
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2024-05-16 11:25:46 +0200

    2017 day 5

--- csharp/AdventOfCode.2017/Challenges/Challenge05.cs
+++ csharp/AdventOfCode.2017/Challenges/Challenge05.cs
@@ -1,20 +1,43 @@
 using AdventOfCode.Core;
 using AdventOfCode.Core.IO;
+using AdventOfCode.Lib;
 
 namespace AdventOfCode2017.Challenges;
 
-//[Challenge(5)]
+[Challenge(5)]
 public class Challenge05(IInputReader inputReader)
 {
     [Part1]
     public async Task<string> Part1Async()
     {
-        return string.Empty;
+        var list = await inputReader.ReadLinesAsync(5).Select(x => x.As<int>()).ToArrayAsync();
+
+        var ip = 0;
+        var steps = 0;
+        while (ip < list.Length)
+        {
+            ip += list[ip]++;
+            steps++;
+        }
+
+        return steps.ToString();
     }
 
-    // [Part2]
+    [Part2]
     public async Task<string> Part2Async()
     {
-        return string.Empty;
+        var list = await inputReader.ReadLinesAsync(5).Select(x => x.As<int>()).ToArrayAsync();
+
+        var ip = 0;
+        var steps = 0;
+        while (ip < list.Length)
+        {
+            var v = ip;
+            ip += list[ip];
+            list[v] += list[v] >= 3 ? -1 : 1;
+            steps++;
+        }
+
+        return steps.ToString();
     }
 }
