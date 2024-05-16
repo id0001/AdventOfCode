8959af868bba835d916b8cb33f1cb271c1e560f9
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2024-05-16 11:42:12 +0200

    2017 day 6

--- csharp/AdventOfCode.2017/Challenges/Challenge06.cs
+++ csharp/AdventOfCode.2017/Challenges/Challenge06.cs
@@ -1,20 +1,74 @@
 using AdventOfCode.Core;
 using AdventOfCode.Core.IO;
+using AdventOfCode.Lib;
+using AdventOfCode.Lib.Collections;
 
 namespace AdventOfCode2017.Challenges;
 
-//[Challenge(6)]
+[Challenge(6)]
 public class Challenge06(IInputReader inputReader)
 {
     [Part1]
     public async Task<string> Part1Async()
     {
-        return string.Empty;
+        var banks = (await inputReader.ReadAllTextAsync(6)).SplitBy("\t").As<int>().ToArray();
+
+        var visited = new HashSet<string> { string.Join(",", banks) };
+        int cycle = 0;
+
+        while (true)
+        {
+            var (i, blocks) = Enumerable.Range(0, 16).Zip(banks).MaxBy(pair => pair.Second);
+            banks[i] = 0;
+
+            while(blocks > 0)
+            {
+                banks[(i + 1).Mod(16)]++;
+                blocks--;
+                i++;
+            }
+
+            cycle++;
+
+            var text = string.Join(",", banks);
+            if (visited.Contains(text))
+                return cycle.ToString();
+
+            visited.Add(text);
+        }
     }
 
-    // [Part2]
+    [Part2]
     public async Task<string> Part2Async()
     {
-        return string.Empty;
+        var banks = (await inputReader.ReadAllTextAsync(6)).SplitBy("\t").As<int>().ToArray();
+
+        var visited = new OrderedDictionary<string, int>
+        {
+            { string.Join(",", banks), 0 }
+        };
+
+        int cycle = 0;
+
+        while (true)
+        {
+            var (i, blocks) = Enumerable.Range(0, 16).Zip(banks).MaxBy(pair => pair.Second);
+            banks[i] = 0;
+
+            while (blocks > 0)
+            {
+                banks[(i + 1).Mod(16)]++;
+                blocks--;
+                i++;
+            }
+
+            cycle++;
+
+            var text = string.Join(",", banks);
+            if (visited.ContainsKey(text))
+                return (cycle - visited[text]).ToString();
+
+            visited.Add(text, cycle);
+        }
     }
 }
