4c07e5f6680bfe334dcae36b5c57bb512e70394c
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2024-06-04 14:01:27 +0200

    2017 day 11

--- csharp/AdventOfCode.2017/Challenges/Challenge11.cs
+++ csharp/AdventOfCode.2017/Challenges/Challenge11.cs
@@ -1,20 +1,35 @@
 using AdventOfCode.Core;
 using AdventOfCode.Core.IO;
+using AdventOfCode.Lib;
 
 namespace AdventOfCode2017.Challenges;
 
-//[Challenge(11)]
+[Challenge(11)]
 public class Challenge11(IInputReader inputReader)
 {
     [Part1]
-    public async Task<string> Part1Async()
-    {
-        return string.Empty;
-    }
+    public async Task<string> Part1Async() => Hex.ManhattanDistance(Hex.Zero, await inputReader.ReadLineAsync(11, ',').AggregateAsync(Hex.Zero, Next)).ToString();
 
-    // [Part2]
-    public async Task<string> Part2Async()
+    [Part2]
+    public async Task<string> Part2Async() => (await inputReader.ReadLineAsync(11, ',')
+            .AggregateAsync((MaxDistance: 0, Hex: Hex.Zero), NextWithMaxDistance))
+            .MaxDistance
+            .ToString();
+
+    private Hex Next(Hex current, string dir) => dir switch
     {
-        return string.Empty;
+        "n" => current.North,
+        "ne" => current.NorthEast,
+        "se" => current.SouthEast,
+        "s" => current.South,
+        "sw" => current.SouthWest,
+        "nw" => current.NorthWest,
+        _ => throw new NotImplementedException()
+    };
+
+    private (int MaxDistance, Hex Hex) NextWithMaxDistance((int MaxDistance, Hex Hex) current, string dir)
+    {
+        var next = Next(current.Hex, dir);
+        return (Math.Max(current.MaxDistance, Hex.ManhattanDistance(Hex.Zero, next)), next);
     }
 }
---
+++ csharp/AdventOfCode.Lib/Hex.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Lib
+{
+    public readonly record struct Hex(int X, int Y, int Z) : IPoint<int>, INeighbors<int>
+    {
+        public static readonly Hex Zero = new Hex();
+
+        public Hex North => new Hex(X, Y - 1, Z + 1);
+        public Hex NorthEast => new Hex(X + 1, Y - 1, Z);
+        public Hex SouthEast => new Hex(X + 1, Y, Z - 1);
+        public Hex South => new Hex(X, Y + 1, Z - 1);
+        public Hex SouthWest => new Hex(X - 1, Y + 1, Z);
+        public Hex NorthWest => new Hex(X - 1, Y, Z + 1);
+
+        IEnumerable<IPoint<int>> INeighbors<int>.GetNeighbors(bool includeDiagonal) =>
+        GetNeighbors().Cast<IPoint<int>>();
+
+        int IPoint<int>.this[int index] => index switch
+        {
+            0 => X,
+            1 => Y,
+            2 => Z,
+            _ => throw new NotSupportedException()
+        };
+
+        int IPoint<int>.Dimensions => 3;
+
+        public bool Equals(IPoint<int>? other)
+        {
+            if (other is null)
+                return false;
+
+            var instance = (IPoint<int>)this;
+
+            if (other.Dimensions != instance.Dimensions)
+                return false;
+
+            for (var d = 0; d < instance.Dimensions; d++)
+                if (instance[d] != other[d])
+                    return false;
+
+            return true;
+        }
+
+        public IEnumerable<Hex> GetNeighbors()
+        {
+            yield return North;
+            yield return NorthEast;
+            yield return SouthEast;
+            yield return South;
+            yield return SouthWest;
+            yield return NorthWest;
+        }
+
+        public void Deconstruct(out int x, out int y, out int z) => (x, y, z) = (X, Y, Z);
+
+        public override string ToString() => $"({X},{Y},{Z})";
+
+        public static int ManhattanDistance(Hex a, Hex b) => (System.Math.Abs(a.X - b.X) + System.Math.Abs(a.Y - b.Y) + System.Math.Abs(a.Z - b.Z)) / 2;
+    }
+}
