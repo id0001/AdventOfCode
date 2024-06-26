367a5fffa58db7cf7313b7628a98bc037a3fa125
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2024-06-26 11:41:58 +0200

    2017 day 15

--- csharp/AdventOfCode.2017/Challenges/Challenge15.cs
+++ csharp/AdventOfCode.2017/Challenges/Challenge15.cs
@@ -1,20 +1,63 @@
 using AdventOfCode.Core;
-using AdventOfCode.Core.IO;
+using AdventOfCode.Lib;
 
 namespace AdventOfCode2017.Challenges;
 
-//[Challenge(15)]
-public class Challenge15(IInputReader inputReader)
+[Challenge(15)]
+public class Challenge15
 {
+    private const int StartA = 783;
+    private const int StartB = 325;
+    private const long Divisor = 2147483647L;
+    private const long FactorA = 16807L;
+    private const long FactorB = 48271L;
+
     [Part1]
-    public async Task<string> Part1Async()
+    public string Part1()
     {
-        return string.Empty;
+        long valueA = StartA;
+        long valueB = StartB;
+        int matches = 0;
+        for (var i = 0; i < 40_000_000; i++)
+        {
+            if (IsMatch(valueA, valueB))
+                matches++;
+
+            valueA = GetNextValueForA(valueA);
+            valueB = GetNextValueForB(valueB);
+        }
+
+        return matches.ToString();
     }
 
-    // [Part2]
-    public async Task<string> Part2Async()
+    [Part2]
+    public string Part2()
     {
-        return string.Empty;
+        long valueA = StartA;
+        long valueB = StartB;
+        int matches = 0;
+        for (var i = 0; i < 5_000_000; i++)
+        {
+            if (IsMatch(valueA, valueB))
+                matches++;
+
+            valueA = GetNextValueForA(valueA, true);
+            valueB = GetNextValueForB(valueB, true);
+        }
+
+        return matches.ToString();
     }
+
+    private static long GetNextValueForA(long current, bool part2 = false) => GetNextValue(current, FactorA, part2 ? 4 : 1);
+    private static long GetNextValueForB(long current, bool part2 = false) => GetNextValue(current, FactorB, part2 ? 8 : 1);
+    private static long GetNextValue(long current, long factor, long mustBeDivisibleBy)
+    {
+        current = (current * factor).Mod(Divisor);
+        while (current.Mod(mustBeDivisibleBy) != 0)
+            current = (current * factor).Mod(Divisor);
+
+        return current;
+    }
+
+    private static bool IsMatch(long a, long b) => (a & 0xFFFF) == (b & 0xFFFF);
 }
