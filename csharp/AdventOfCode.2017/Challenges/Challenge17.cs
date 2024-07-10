bf8c3177af20d2c0bfd9d6e8044d2341708a8622
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2024-07-10 13:55:19 +0200

    2017 day 17

--- csharp/AdventOfCode.2017/Challenges/Challenge17.cs
+++ csharp/AdventOfCode.2017/Challenges/Challenge17.cs
@@ -1,20 +1,50 @@
 using AdventOfCode.Core;
-using AdventOfCode.Core.IO;
+using AdventOfCode.Lib;
 
 namespace AdventOfCode2017.Challenges;
 
-//[Challenge(17)]
-public class Challenge17(IInputReader inputReader)
+[Challenge(17)]
+public class Challenge17
 {
+    private const int Input = 304;
+
     [Part1]
-    public async Task<string> Part1Async()
+    public string Part1()
     {
-        return string.Empty;
+        var list = new LinkedList<int>();
+        list.AddLast(0);
+
+        var current = list.First!;
+        for (var i = 0; i < 2017; i++)
+        {
+            int steps = Input % list.Count;
+            for (var j = 0; j < steps; j++)
+                current = current.NextOrFirst();
+
+            list.AddAfter(current, i + 1);
+            current = current.NextOrFirst();
+        }
+
+
+        return list.Find(2017)!.NextOrFirst().Value.ToString();
     }
 
-    // [Part2]
-    public async Task<string> Part2Async()
+    [Part2]
+    public string Part2()
     {
-        return string.Empty;
+        var count = 1;
+        var currentIndex = 0;
+        var result = 0;
+
+        for (var i = 0; i < 50_000_000; i++)
+        {
+            currentIndex = (currentIndex + Input).Mod(count) + 1;
+            if (currentIndex == 1) // this currently is the answer
+                result = i + 1;
+
+            count++;
+        }
+
+        return result.ToString();
     }
 }
---
+++ csharp/AdventOfCode.Lib/Extensions/LinkedListExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace AdventOfCode.Lib
+{
+    public static class LinkedListExtensions
+    {
+        public static LinkedListNode<T> NextOrFirst<T>(this LinkedListNode<T> current)
+        {
+            if (current.List is null)
+                throw new ArgumentException("Node is unlinked");
+
+            return current.Next ?? current.List.First!;
+        }
+
+        public static LinkedListNode<T> PreviousOrLast<T>(this LinkedListNode<T> current)
+        {
+            if (current.List is null)
+                throw new ArgumentException("Node is unlinked");
+
+            return current.Previous ?? current.List!.Last!;
+        }
+    }
+}
