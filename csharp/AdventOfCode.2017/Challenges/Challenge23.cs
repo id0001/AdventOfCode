0eb6eaa519c8a1efa7a8d6c66746d285c6afcf77
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2024-08-08 17:23:20 +0200

    2017 day 23

--- csharp/AdventOfCode.2017/Challenges/Challenge23.cs
+++ csharp/AdventOfCode.2017/Challenges/Challenge23.cs
@@ -1,20 +1,58 @@
 using AdventOfCode.Core;
 using AdventOfCode.Core.IO;
+using AdventOfCode.Lib;
+using AdventOfCode.Lib.Assembly;
+using System.Net.NetworkInformation;
 
 namespace AdventOfCode2017.Challenges;
 
-//[Challenge(23)]
+[Challenge(23)]
 public class Challenge23(IInputReader inputReader)
 {
     [Part1]
     public async Task<string> Part1Async()
     {
-        return string.Empty;
+        var program = await inputReader.ParseLinesAsync(23, ParseLine).ToListAsync();
+
+        var cpu = new Cpu<MemoryEx, Arguments>(new(), program);
+        cpu.AddInstruction("set", (args, mem) => { mem.Set(args.X, args.Y.Value(mem)); mem.Ip++; });
+        cpu.AddInstruction("sub", (args, mem) => { mem.Set(args.X, args.X.Value(mem) - args.Y.Value(mem)); mem.Ip++; });
+        cpu.AddInstruction("mul", (args, mem) => { mem.Set(args.X, args.X.Value(mem) * args.Y.Value(mem)); mem.Ip++; mem.MulCalled++; });
+        cpu.AddInstruction("jnz", (args, mem) => mem.Ip += args.X.Value(mem) != 0 ? args.Y.Value(mem) : 1);
+
+        cpu.RunTillHalted();
+        return cpu.Memory.MulCalled.ToString();
     }
 
-    // [Part2]
+    [Part2]
     public async Task<string> Part2Async()
     {
-        return string.Empty;
+        var b = (57 * 100) + 100000;
+        var c = b + 17000;
+        var h = 0;
+
+        for(var i = b; i <= c; i+= 17)
+        {
+            var d = 2;
+            while (i % d != 0)
+                d++;
+
+            if (i != d)
+                h++;
+        }
+
+        return h.ToString();
     }
+
+    private static Instruction<Arguments> ParseLine(string line) => line
+        .SplitBy(" ")
+        .Into(parts => new Instruction<Arguments>(parts.First(), new Arguments(parts.Second(), parts.Third())));
+
+    private record Arguments(RegisterOrValueArgument<int> X, RegisterOrValueArgument<int> Y);
+
+    private class MemoryEx : RegisterMemory<int>
+    {
+
+        public int MulCalled { get; set; }
+    };
 }
