fe90b04d26cbf83f768806361e2f5a20e56bd1cb
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2024-09-23 16:00:21 +0200

    2018 day 6

--- csharp/AdventOfCode.2018/Challenges/Challenge06.cs
+++ csharp/AdventOfCode.2018/Challenges/Challenge06.cs
@@ -1,20 +1,78 @@
 using AdventOfCode.Core;
 using AdventOfCode.Core.IO;
+using AdventOfCode.Lib;
+using AdventOfCode.Lib.Collections;
+using AdventOfCode.Lib.Collections.Helpers;
+using System.Diagnostics;
+using System.Net.WebSockets;
 
 namespace AdventOfCode2018.Challenges;
 
-//[Challenge(6)]
+[Challenge(6)]
 public class Challenge06(IInputReader inputReader)
 {
     [Part1]
     public async Task<string> Part1Async()
     {
-        return string.Empty;
+        var coords = await inputReader.ParseLinesAsync(6, ParseLine).ToListAsync();
+        var cloud = new PointCloud<Point2, int>(coords);
+        var areas = new Dictionary<Point2, int>();
+        foreach (var p in cloud.Bounds.EnumeratePoints())
+        {
+            if (!TryGetClosestPoint(p, coords, out var closest))
+                continue;
+
+            areas.TryAdd(closest, 0);
+            if (IsOnBorder(p, cloud.Bounds))
+                areas[closest] = int.MaxValue;
+            else
+                areas[closest] = Math.Min(areas[closest] + 1, int.MaxValue);
+        }
+
+        return areas.Where(kv => kv.Value != int.MaxValue).Max(kv => kv.Value).ToString();
     }
 
-    // [Part2]
+    [Part2]
     public async Task<string> Part2Async()
     {
-        return string.Empty;
+        var coords = await inputReader.ParseLinesAsync(6, ParseLine).ToListAsync();
+        var cloud = new PointCloud<Point2, int>(coords);
+        int totalArea = 0;
+        foreach (var p in cloud.Bounds.EnumeratePoints())
+        {
+            var sumOfDistances = coords.Sum(c => Point2.ManhattanDistance(p, c));
+            if (sumOfDistances < 10000)
+                totalArea++;
+        }
+
+        return totalArea.ToString();
     }
+
+    private static bool IsOnBorder(Point2 p, BoundingBox<Point2, int> bounds)
+        => p.X == bounds.GetMin(0)
+        || p.X == bounds.GetMax(0)
+        || p.Y == bounds.GetMin(1)
+        || p.Y == bounds.GetMax(1);
+
+    private static bool TryGetClosestPoint(Point2 point, IList<Point2> points, out Point2 closest)
+    {
+        closest = Point2.Zero;
+        var first2ClosestPoints = points
+            .Select(p => (Point: p, Distance: Point2.ManhattanDistance(point, p)))
+            .OrderBy(p => p.Distance)
+            .Take(2)
+            .ToArray();
+
+        // Ignore point if it's closest to at least 2 points
+        if (first2ClosestPoints.First().Distance == first2ClosestPoints.Second().Distance)
+            return false;
+
+        closest = first2ClosestPoints.First().Point;
+        return true;
+    }
+
+    private static Point2 ParseLine(string line) => line
+        .SplitBy(",")
+        .As<int>()
+        .Into(coords => new Point2(coords.First(), coords.Second()));
 }
---
+++ csharp/AdventOfCode.Lib/Extensions/BoundingBoxExtensions.cs
@@ -0,0 +1,53 @@
+﻿using AdventOfCode.Lib.Collections.Helpers;
+using Microsoft;
+
+namespace AdventOfCode.Lib
+{
+    public static class BoundingBoxExtensions
+    {
+        public static IEnumerable<Point2> EnumeratePoints(this BoundingBox<Point2, int> box)
+        {
+            var minBounds = Enumerable.Range(0, box.Dimensions).Select(box.GetMin).ToArray();
+            var maxBounds = Enumerable.Range(0, box.Dimensions).Select(box.GetMax).ToArray();
+
+            foreach (var point in IterateOverPoints(minBounds, maxBounds))
+                yield return new Point2(point[0], point[1]);
+        }
+
+        public static IEnumerable<Point3> EnumeratePoints(this BoundingBox<Point3, int> box)
+        {
+            var minBounds = Enumerable.Range(0, box.Dimensions).Select(box.GetMin).ToArray();
+            var maxBounds = Enumerable.Range(0, box.Dimensions).Select(box.GetMax).ToArray();
+
+            foreach (var point in IterateOverPoints(minBounds, maxBounds))
+                yield return new Point3(point[0], point[1], point[2]);
+        }
+
+        private static IEnumerable<int[]> IterateOverPoints(int[] minBounds, int[] maxBounds)
+        {
+            Requires.Argument(minBounds.Length == maxBounds.Length, null, "Min and max bounds must have the same length");
+
+            int dimensions = minBounds.Length;
+            int[] currentPoint = new int[dimensions];
+
+            IEnumerable<int[]> Recurse(int dimension)
+            {
+                if (dimension == dimensions)
+                {
+                    yield return (int[])currentPoint.Clone();
+                    yield break;
+                }
+
+                for (var i = minBounds[dimension]; i <= maxBounds[dimension]; i++)
+                {
+                    currentPoint[dimension] = i;
+                    foreach (var point in Recurse(dimension + 1))
+                        yield return point;
+                }
+            }
+
+            foreach (var point in Recurse(0))
+                yield return point;
+        }
+    }
+}
