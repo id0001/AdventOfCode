47f38b98d14339d5d2a5ae020cd98fd4d17756ee
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2024-09-26 11:54:07 +0200

    2018 day 8

--- csharp/AdventOfCode.2018/Challenges/Challenge08.cs
+++ csharp/AdventOfCode.2018/Challenges/Challenge08.cs
@@ -1,20 +1,79 @@
 using AdventOfCode.Core;
 using AdventOfCode.Core.IO;
+using AdventOfCode.Lib.Collections.Trees;
+using AdventOfCode.Lib.Extensions;
+using JetBrains.Annotations;
+using System.Xml.Linq;
 
 namespace AdventOfCode2018.Challenges;
 
-//[Challenge(8)]
+[Challenge(8)]
 public class Challenge08(IInputReader inputReader)
 {
     [Part1]
     public async Task<string> Part1Async()
     {
-        return string.Empty;
+        var input = await inputReader.ReadLineAsync<int>(8, ' ').ToArrayAsync();
+
+        var (root, _) = CreateTreeNode(input, 0);
+        return root.EnumeratePreOrder().Sum(x => x.Value.Sum()).ToString();
     }
 
-    // [Part2]
+    [Part2]
     public async Task<string> Part2Async()
     {
-        return string.Empty;
+        var input = await inputReader.ReadLineAsync<int>(8, ' ').ToArrayAsync();
+
+        var (root, _) = CreateTreeNode(input, 0);
+
+        var stack = new Stack<GeneralTreeNode<int[]>>();
+        stack.Push(root);
+
+        int sum = 0;
+        while (stack.Count > 0)
+        {
+            var current = stack.Pop();
+
+            if (current.ChildCount == 0)
+            {
+                sum += current.Value.Sum();
+                continue;
+            }
+
+            var children = current.Children.ToList();
+            foreach (var i in current.Value)
+            {
+                if (i - 1 < children.Count && i - 1 >= 0)
+                    stack.Push(children[i - 1]);
+            }
+        }
+
+        return sum.ToString();
+    }
+
+    private (GeneralTreeNode<int[]>, int) CreateTreeNode(int[] input, int left)
+    {
+        var childCount = input[left];
+        var lengthMetadata = input[left + 1];
+
+
+        var children = new List<GeneralTreeNode<int[]>>();
+
+        var totalChildrenLength = 0;
+        left += 2; // start of first child
+        for (var i = 0; i < childCount; i++)
+        {
+            var (child, len) = CreateTreeNode(input, left + totalChildrenLength);
+            children.Add(child);
+            totalChildrenLength += len;
+        }
+
+        left += totalChildrenLength;
+        var node = new GeneralTreeNode<int[]>(input[left..(left + lengthMetadata)]);
+
+        foreach (var child in children)
+            node.AddChild(child);
+
+        return (node, totalChildrenLength + 2 + lengthMetadata);
     }
 }
---
+++ csharp/AdventOfCode.Lib/Extensions/GeneralTreeNodeExtensions.cs
@@ -0,0 +1,37 @@
+﻿using AdventOfCode.Lib.Collections.Trees;
+
+namespace AdventOfCode.Lib.Extensions
+{
+    public static class GeneralTreeNodeExtensions
+    {
+        public static void MakeRoot<T>(this GeneralTreeNode<T> source)
+        {
+            if (source.Parent == null)
+                return;
+
+            var parent = source.Parent;
+            parent.RemoveChild(source);
+            parent.MakeRoot();
+            source.AddChild(parent);
+        }
+
+        public static IEnumerable<GeneralTreeNode<T>> EnumeratePreOrder<T>(this GeneralTreeNode<T> node)
+        {
+            if (node is null)
+                yield break;
+
+            var stack = new Stack<GeneralTreeNode<T>>();
+            stack.Push(node);
+
+            while (stack.Count > 0)
+            {
+                var current = stack.Pop();
+
+                yield return current;
+
+                foreach (var child in current.Children.Reverse())
+                    stack.Push(child);
+            }
+        }
+    }
+}
