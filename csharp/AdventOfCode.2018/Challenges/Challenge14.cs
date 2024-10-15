a56fff894703e8582da2a7d808dbed56e906bb1d
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2024-10-15 15:57:53 +0200

    Optimized a bit

--- csharp/AdventOfCode.2018/Challenges/Challenge14.cs
+++ csharp/AdventOfCode.2018/Challenges/Challenge14.cs
@@ -12,17 +12,19 @@ public class Challenge14(IInputReader inputReader)
     {
         var input = await inputReader.ReadNumberAsync<int>(14);
 
-        var indices = new[] { 0, 1 };
+        var (a, b) = (0, 1);
         var list = new List<int> { 3, 7 };
 
         for (var i = 0; list.Count < input + 10; i++)
         {
-            var sum = list[indices[0]] + list[indices[1]];
-            foreach (var c in sum.ToString())
-                list.Add((int)char.GetNumericValue(c));
+            var sum = list[a] + list[b];
 
-            indices[0] = (indices[0] + 1 + list[indices[0]]).Mod(list.Count);
-            indices[1] = (indices[1] + 1 + list[indices[1]]).Mod(list.Count);
+            if (sum >= 10)
+                list.Add(sum / 10); // First digit
+            list.Add(sum % 10); // Second or only digit
+
+            a = (a + 1 + list[a]) % list.Count;
+            b = (b + 1 + list[b]) % list.Count;
         }
 
         return string.Concat(list[^10..]);
@@ -31,27 +33,24 @@ public class Challenge14(IInputReader inputReader)
     [Part2]
     public async Task<string> Part2Async()
     {
-        var input = await inputReader.ReadAllTextAsync(14);
-        var inputNumbers = input.Select(c => (int)char.GetNumericValue(c)).ToArray();
+        var input = (await inputReader.ReadNumberAsync<int>(14)).EnumerateDigits().ToArray();
 
-        var indices = new[] { 0, 1 };
+        var (a, b) = (0, 1);
         var list = new List<int> { 3, 7 };
 
         while (true)
         {
-            var sum = (list[indices[0]] + list[indices[1]]).ToString();
+            var sum = list[a] + list[b];
 
-            foreach (var c in sum)
-                list.Add((int)char.GetNumericValue(c));
+            if (sum >= 10)
+                list.Add(sum / 10); // First digit
+            list.Add(sum % 10); // Second or only digit
 
-            indices[0] = (indices[0] + 1 + list[indices[0]]).Mod(list.Count);
-            indices[1] = (indices[1] + 1 + list[indices[1]]).Mod(list.Count);
+            a = (a + 1 + list[a]) % list.Count;
+            b = (b + 1 + list[b]) % list.Count;
 
-            if (list.Count > input.Length && sum[0] == input[^1])
-            {
-                if (list[^(input.Length + 1)..^1].SequenceEqual(inputNumbers))
-                    return (list.Count - input.Length - 1).ToString();
-            }
+            if (list.Count > input.Length && (sum >= 10 ? list[^2] : list[^1]) == input[^1] && list[^(input.Length + 1)..^1].SequenceEqual(input))
+                return (list.Count - input.Length - 1).ToString();
         }
     }
 }
