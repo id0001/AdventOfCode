f22a60758371d836644a9cc13c0545533426d3c4
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2024-11-06 14:32:58 +0100

    Added path find extension for DirectedGraph

---
+++ csharp/AdventOfCode.Lib/Extensions/Graphs/DirectedGraphExtensions.Astar.cs
@@ -0,0 +1,27 @@
+﻿using AdventOfCode.Lib.Graphs;
+using AdventOfCode.Lib.PathFinding;
+
+namespace AdventOfCode.Lib
+{
+    public static partial class DirectedGraphExtensions
+    {
+        public static bool TryPath<TVertex>(this DirectedGraph<TVertex, int> graph, TVertex start, Func<TVertex, bool> isFinished, out IEnumerable<TVertex> path, out int totalCost)
+            where TVertex : notnull
+        {
+            var astar = new AStar<TVertex>(c => GetAdjacent(graph, c), (c, n) => GetWeight(graph, c, n));
+            return astar.TryPath(start, isFinished, out path, out totalCost);
+        }
+
+        private static IEnumerable<TVertex> GetAdjacent<TVertex, TEdge>(DirectedGraph<TVertex, TEdge> graph, TVertex current)
+            where TVertex : notnull
+            where TEdge : notnull
+        {
+            foreach (var edges in graph.OutEdges(current))
+                yield return edges.Key;
+        }
+
+        private static int GetWeight<TVertex>(DirectedGraph<TVertex, int> graph, TVertex current, TVertex next)
+            where TVertex : notnull
+            => graph.OutEdges(current)[next];
+    }
+}
