026dc3930ebb29ff2b7f2504f7a6bbffbc903edf
Yorrick Spierings <yi.spierings@gmail.com>
2024-12-01 06:17:23 +0100

    2024 day 1

--- csharp/AdventOfCode.2024/Challenges/Challenge01.cs
+++ csharp/AdventOfCode.2024/Challenges/Challenge01.cs
@@ -1,5 +1,6 @@
 using AdventOfCode.Core;
 using AdventOfCode.Core.IO;
+using AdventOfCode.Lib;
 
 namespace AdventOfCode2024.Challenges;
 
@@ -9,14 +10,31 @@ public class Challenge01(IInputReader inputReader)
     [Part1]
     public async Task<string> Part1Async()
     {
-        return string.Empty;
+        var (a, b) = await inputReader.ParseTextAsync(1, ParseText);
+        return a.Zip(b).Select(x => Math.Abs(x.First - x.Second)).Sum().ToString();
     }
 
-    // [Part2]
+    [Part2]
     public async Task<string> Part2Async()
     {
-        return string.Empty;
+        var (a, b) = await inputReader.ParseTextAsync(1, ParseText);
+        var lookup = b.GroupBy(x => x).ToDictionary(kv => kv.Key, kv => kv.Count());
+        return a.Select(x => lookup.GetValueOrDefault(x,0) * x).Sum().ToString();
     }
 
-    private record GroupTemplate(string Army, int Id, int Units, int Health, int Damage);
+    private static (List<int>, List<int>) ParseText(string input)
+    {
+        var a = new List<int>();
+        var b = new List<int>();
+
+        foreach(var (first, second) in input.Lines().Select(line => line.SplitBy<int,int>(" ")))
+        {
+            a.Add(first);
+            b.Add(second);
+        }
+
+        a.Sort();
+        b.Sort();
+        return (a, b);
+    }
 }
