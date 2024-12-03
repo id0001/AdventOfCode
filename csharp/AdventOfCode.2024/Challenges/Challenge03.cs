31232000a88337d35c33fc7e0ddc5dee4b56afde
= <yi.spierings@gmail.com>
2024-12-03 06:21:46 +0100

    2024 day 3

--- csharp/AdventOfCode.2024/Challenges/Challenge03.cs
+++ csharp/AdventOfCode.2024/Challenges/Challenge03.cs
@@ -1,20 +1,51 @@
+using System.Text.RegularExpressions;
 using AdventOfCode.Core;
 using AdventOfCode.Core.IO;
+using AdventOfCode.Lib;
 
 namespace AdventOfCode2024.Challenges;
 
-//[Challenge(3)]
+[Challenge(3)]
 public class Challenge03(IInputReader inputReader)
 {
     [Part1]
     public async Task<string> Part1Async()
     {
-        return string.Empty;
+        var input = await inputReader.ReadAllTextAsync(3);
+        var mulls = input.ExtractAll(@"mul\((\d+),(\d+)\)");
+        return mulls.Sum(digits => digits.As<int>().Product()).ToString();
     }
 
-    // [Part2]
+    [Part2]
     public async Task<string> Part2Async()
     {
-        return string.Empty;
+        var enabled = true;
+        var input = await inputReader.ReadAllTextAsync(3);
+        var sum = 0;
+        for (var i = 0; i < input.Length; i++)
+        {
+            if (input[i] != ')')
+                continue; // Optimized
+
+            if (input[..(i + 1)].EndsWith("don't()"))
+            {
+                enabled = false;
+                continue;
+            }
+
+            if (input[..(i + 1)].EndsWith("do()"))
+            {
+                enabled = true;
+                continue;
+            }
+
+            if(!enabled)
+                continue;
+
+            if (input[..(i + 1)].TryExtract<int>(@"mul\((\d+),(\d+)\)$", out var numbers))
+                sum += numbers.Product();
+        }
+
+        return sum.ToString();
     }
 }
