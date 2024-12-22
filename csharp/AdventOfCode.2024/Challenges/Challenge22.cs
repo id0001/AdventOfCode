9e6624f52a6712a5d44659e19b1085dd85cd9093
= <yi.spierings@gmail.com>
2024-12-22 07:43:31 +0100

    2024 day 22

--- csharp/AdventOfCode.2024/Challenges/Challenge22.cs
+++ csharp/AdventOfCode.2024/Challenges/Challenge22.cs
@@ -3,18 +3,79 @@ using AdventOfCode.Core.IO;
 
 namespace AdventOfCode2024.Challenges;
 
-//[Challenge(22)]
+[Challenge(22)]
 public class Challenge22(IInputReader inputReader)
 {
     [Part1]
     public async Task<string> Part1Async()
     {
-        return string.Empty;
+        var input = await inputReader.ReadLinesAsync<long>(22).ToListAsync();
+        long sum = 0;
+
+        for (var n = 0; n < input.Count; n++)
+        {
+            var secret = input[n];
+            for (var i = 0; i < 2000; i++)
+                secret = CalculateSecret(secret);
+
+            sum += secret;
+        }
+
+        return sum.ToString();
     }
 
-    // [Part2]
+    [Part2]
     public async Task<string> Part2Async()
     {
-        return string.Empty;
+        var input = await inputReader.ReadLinesAsync<long>(22).ToListAsync();
+
+        var total = new Dictionary<(long, long, long, long), long>();
+
+        for(var n = 0; n < input.Count; n++)
+        {
+            var secret = input[n];
+            var values = new long[2000];
+            var changes = new long[2000];
+            long previous = 0;
+            var seenSequences = new HashSet<(long,long,long,long)>();
+
+            for (var i = 0; i < 2000; i++)
+            {
+                secret = CalculateSecret(secret);
+                values[i] = secret % 10;
+                changes[i] = (secret % 10) - previous;
+                previous = secret % 10;
+            }
+
+            for (var i = 3; i < 2000; i++)
+            {
+                var key = (changes[i - 3], changes[i - 2], changes[i - 1], changes[i]);
+                if (!seenSequences.Contains(key))
+                {
+                    seenSequences.Add(key);
+                    if (!total.TryAdd(key, values[i]))
+                        total[key] += values[i];
+                }
+            }
+        }
+        
+        return total.Values.Max().ToString();
+    }
+
+    private static long CalculateSecret(long secret)
+    {
+        var a = 64L * secret;
+        secret ^= a;
+        secret %= 16777216L;
+
+        var b = secret / 32L;
+        secret ^= b;
+        secret %= 16777216L;
+
+        var c = secret * 2048;
+        secret ^= c;
+        secret %= 16777216L;
+
+        return secret;
     }
 }
