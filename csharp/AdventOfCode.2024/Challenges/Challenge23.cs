2bc415638a8dbeb5e486ccd3901c88edede12655
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2024-12-23 12:40:00 +0100

    2024 day 23

--- csharp/AdventOfCode.2024/Challenges/Challenge23.cs
+++ csharp/AdventOfCode.2024/Challenges/Challenge23.cs
@@ -1,20 +1,67 @@
 using AdventOfCode.Core;
 using AdventOfCode.Core.IO;
+using AdventOfCode.Lib;
+using AdventOfCode.Lib.Graphs;
 
 namespace AdventOfCode2024.Challenges;
 
-//[Challenge(23)]
+[Challenge(23)]
 public class Challenge23(IInputReader inputReader)
 {
     [Part1]
     public async Task<string> Part1Async()
     {
-        return string.Empty;
+        var graph = new UndirectedGraph<string, int>();
+        var edges = await inputReader.ParseLinesAsync(23, ParseLine).ToListAsync();
+        foreach (var edge in edges)
+        {
+            graph.AddVertex(edge[0]);
+            graph.AddVertex(edge[1]);
+            graph.AddEdge(edge[0], edge[1], 1);
+        }
+
+        return EnumerateCyclesOf3(edges, graph)
+            .Count(verts => verts.Any(v => v.StartsWith("t")))
+            .ToString();
     }
 
-    // [Part2]
+    [Part2]
     public async Task<string> Part2Async()
     {
-        return string.Empty;
+        var graph = new UndirectedGraph<string, int>();
+        await foreach (var edge in inputReader.ParseLinesAsync(23, ParseLine))
+        {
+            graph.AddVertex(edge[0]);
+            graph.AddVertex(edge[1]);
+            graph.AddEdge(edge[0], edge[1], 1);
+        }
+
+        return string.Join(",", graph.EnumerateCliques().OrderByDescending(c => c.Count).First().Order());
     }
+
+    private static IEnumerable<string[]> EnumerateCyclesOf3(IEnumerable<string[]> edges, UndirectedGraph<string, int> graph)
+    {
+        var visited = new HashSet<(string, string, string)>();
+        foreach (var edge in edges)
+        {
+            foreach (var c0 in graph.AdjacentEdges(edge[1]).Keys)
+            {
+                foreach (var c1 in graph.AdjacentEdges(c0).Keys)
+                {
+                    if (c1 == edge[0])
+                    {
+                        var v = new[] { edge[0], edge[1], c0 }.Order().ToArray();
+                        var key = (v[0], v[1], v[2]);
+                        if (visited.Contains(key))
+                            continue;
+
+                        visited.Add(key);
+                        yield return v;
+                    }
+                }
+            }
+        }
+    }
+
+    private static string[] ParseLine(string line) => line.SplitBy("-").ToArray();
 }
---
+++ csharp/AdventOfCode.Lib/Extensions/Graphs/UndirectedGraphExtensions.EnumerateCliques.cs
@@ -0,0 +1,40 @@
+﻿using AdventOfCode.Lib.Graphs;
+using System.Collections.Immutable;
+
+namespace AdventOfCode.Lib
+{
+    public static partial class UndirectedGraphExtensions
+    {
+        public static List<List<TVertex>> EnumerateCliques<TVertex, TEdge>(this UndirectedGraph<TVertex, TEdge> source)
+            where TVertex : notnull
+            where TEdge : notnull
+        {
+            var r = ImmutableHashSet<TVertex>.Empty;
+            var p = source.Vertices.ToImmutableHashSet();
+            var x = ImmutableHashSet<TVertex>.Empty;
+
+            var cliques = new List<List<TVertex>>();
+            BronKerbosch(source, cliques, r, p, x);
+            return cliques;
+        }
+
+        private static void BronKerbosch<TVertex, TEdge>(UndirectedGraph<TVertex, TEdge> graph, List<List<TVertex>> cliques, ImmutableHashSet<TVertex> r, ImmutableHashSet<TVertex> p, ImmutableHashSet<TVertex> x)
+            where TVertex : notnull
+            where TEdge : notnull
+        {
+            if (p.Count == 0 && x.Count == 0)
+            {
+                cliques.Add([.. r]);
+                return;
+            }
+
+            var pivot = p.Union(x).First();
+            foreach (var v in p.Except(graph.AdjacentEdges(pivot).Keys))
+            {
+                BronKerbosch(graph, cliques, r.Add(v), p.Intersect(graph.AdjacentEdges(v).Keys), x.Intersect(graph.AdjacentEdges(v).Keys));
+                p = p.Remove(v);
+                x = x.Add(v);
+            }
+        }
+    }
+}
